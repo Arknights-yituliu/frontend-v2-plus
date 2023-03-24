b8c08271793c0e4c2f26996d7804c1686d36ddf4
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-03-24 18:42:04 +0800

    删除SSR相关的多余console.log

--- src/renderer/_default.page.client.js
+++ src/renderer/_default.page.client.js
@@ -4,7 +4,6 @@ import { createVPSApp } from "./app";
 
 async function render(pageContext) {
   const clientOnly = document.getElementById("client_only") != null;
-  console.log(clientOnly);
   const app = createVPSApp(pageContext, clientOnly);
   app.mount("#app");
 }
