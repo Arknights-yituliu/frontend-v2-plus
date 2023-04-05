123d89d9da1f13aa8617e6e3f4c424a37070c37f
Zirun-wang <zirunwang.g@gmail.com>
2023-04-06 05:23:56 +0800

    修改了114514处样式

--- src/pages/ep12.page.vue
+++ src/pages/ep12.page.vue
@@ -1,21 +1,35 @@
 <template>
   <div id="ep12" style="font-size: 20px;">
-    <div style="height: 280px;">占位</div>
-    <div>
-      <img src="\public\img\website\ico64.png">
-      明日方舟一图流 EP12专题页面
+    <!-- 占位 -->
+    <div style="height: 192px;"></div>
+    <!-- 标题 -->
+    <div style="text-align: center;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
+      <img src="\public\img\website\ico64.png" style="border-radius: 8px;">
+      <div style="font-size: 32px;vertical-align: top;margin:12px;display: inline-block;">
+        明日方舟一图流 EP12专题页面
+      </div>
     </div>
-    <div>
-      本页面上半部分集中展示新章节的掉落数据，下半部分推荐[后勤特别许可证]的使用优先级
+    <!-- 简单说明 -->
+    <div style="padding:12px;font-style: italic;line-height: 32px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
+      上半页集中展示新章节<b>掉落数据</b><br>
+      下半页推荐<b>[后勤特别许可证]</b>的使用优先级
     </div>
-    <div style="height: 400px;">
-      数据补全计划
-      <img src="public\img\temp\ep12_drb.png" style="max-width: 680px;margin: auto;">
-      新章节开放时往往伴有明显的马太效应，即刷的人越多，数据越可靠，越能吸引更多的博士去刷；反之亦然。
-      为了找到隐藏的高效率关卡，我们联合！缺少样本的关卡，我们碎石刷！
-      同时也欢迎大家参与提交数据
+    <!-- 数据补全计划 -->
+    <div style="background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
+      <div style="text-align: center;font-size: 24px;font-weight: 600;padding-top: 6px;">
+        数据补全计划
+      </div>
+      <div style="margin: 0px 6px;">
+        <img src="public\img\temp\ep12_drb.png" style="width: 100%;">
+      </div>
+      <div style="font-style: italic;padding: 0px 12px 12px 12px;">
+      新章节开放时往往伴有明显的马太效应，即刷的人越多，数据越可靠，越能吸引更多的博士去刷；反之亦然。<br>
+      为了找到隐藏的高效率关卡，我们联合！缺少样本的关卡，我们碎石刷！<br>
+      同时欢迎大家参与提交数据！
+      </div>
     </div>
-    <div style="height: 400px;">
+    <!-- 数据补全计划表格 -->
+    <div style="height: 400px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
       <table class="popup_table">
         <tbody>
           <tr class="popup_table_title">
@@ -57,9 +71,21 @@
         </tbody>
       </table>
     </div>
-    <div>
-      [后勤特别许可证]<br>
-      ◆本次活动新增后勤特别许可证：在【惊霆无声】任一可三星代理作战的关卡中，每日前两次作战可使用后勤特别许可证，使用后的当次作战不消耗理智且原常规掉落的报酬改为固定掉落且数量增加至三倍
+    <!-- 后勤特别许可证 -->
+    <div style="height: 192px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
+      <div style="text-align: center;font-size: 24px;font-weight: 600;">
+        后勤特别许可证
+      </div>
+      <div style="vertical-align: top;height: 144px;">
+        <div style="width: 96px;height: 96px;background-color: aqua;display: inline-block;vertical-align: top;margin: 24px 12px 24px 12px;">
+          此处图片
+        </div>
+        <div style="display: inline-block;max-width: 402px;">
+          ◆本次活动新增后勤特别许可证：<br>
+          在【惊霆无声】任一可三星代理作战的关卡中，每日前两次作战可使用后勤特别许可证，使用后的当次作战不消耗理智且原常规掉落的报酬改为固定掉落且数量增加至三倍
+        </div>
+      </div>
+      
     </div>
     <div>
       推荐表-占位<br>
@@ -72,12 +98,41 @@
       【这里放价值表】
     </div>
     <div>
-      <div class="ep12_support">巴别塔攻略组<br>（数据提供）</div>
-      <div class="ep12_support">企鹅物流数据统计<br>（数据收集）</div>
-      <div class="ep12_support">明日方舟一图流<br>（数据分析） </div>
-      <div class="ep12_support">罗德岛基建BETA<br>（信息发布） </div>
-      <div class="ep12_support">xxxx<br>（页面设计）</div>
-        
+      <div class="ep12_support">
+        <img class="ep12_support_img" src="public\img\website\babel.jpg">
+        <div class="ep12_support_text">
+          <div class="ep12_support_org">巴别塔攻略组</div>
+          <div class="ep12_support_role">数据提供</div>
+        </div>
+      </div>
+      <div class="ep12_support">
+        <img class="ep12_support_img" src="public\img\website\penguin_stats_logo.png">
+        <div class="ep12_support_text">
+          <div class="ep12_support_org">企鹅物流数据统计</div>
+          <div class="ep12_support_role">数据收集</div>
+        </div>
+      </div>
+      <div class="ep12_support">
+        <img class="ep12_support_img" src="public\img\website\ico64.png">
+        <div class="ep12_support_text">
+          <div class="ep12_support_org">明日方舟一图流</div>
+          <div class="ep12_support_role">数据分析</div>
+        </div>
+      </div>
+      <div class="ep12_support">
+        <img class="ep12_support_img" src="public\img\website\beta_logo_yw.png">
+        <div class="ep12_support_text">
+          <div class="ep12_support_org">罗德岛基建BETA</div>
+          <div class="ep12_support_role">信息发布</div>
+        </div>
+      </div>
+      <div class="ep12_support">
+        <img class="ep12_support_img" src="public\img\website\beta_logo_yw.png">
+        <div class="ep12_support_text">
+          <div class="ep12_support_org">xxxx</div>
+          <div class="ep12_support_role">页面设计</div>
+        </div>
+      </div>        
     </div>
   </div>
 </template>
@@ -108,12 +163,28 @@ export default {
   color:antiquewhite;
 }
 
-#ep12 div{
-  margin: 12px;
-  border: 1px solid #FFFFFF;
-}
+
 
 .ep12_support{
+  width: 258px;
+  height: 96px;
   display: inline-block;
 }
+.ep12_support_img{
+  width: 96px;
+  margin: 0px;
+}
+.ep12_support_text{
+  width: 160px;
+  font-size: 20px;
+  display: inline-block;
+  height: 90px;
+  vertical-align: bottom;
+}
+.ep12_support_text div{
+  height: 36px;
+  text-align: center;
+}
+
+
 </style>
