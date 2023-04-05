3f150ccd5b43da79484ddc5b7fc5fe62382412fd
Zirun-wang <zirunwang.g@gmail.com>
2023-04-06 00:20:53 +0800

    补充信息

--- src/pages/ep12.page.vue
+++ src/pages/ep12.page.vue
@@ -2,14 +2,14 @@
   <div id="ep12">
     <div >明日方舟一图流 EP12专题页面</div>
     <div>
-      说明模块<br>
-      本页面主要包括两部分，一部分用于展示新章节的掉落数据，另一部分用于推荐每天嫖两次的那个券怎么用
+      本页面上半部分集中展示新章节的掉落数据，下半部分推荐[后勤特别许可证]的使用优先级
     </div>
     <div style="height: 400px;">
       数据补全计划<br>
       【这里放图片】<br>
-      新章节开放时往往会有很明显的马太效应，刷的人越多，数据越可靠，越能吸引更多的博士去刷；反之亦然。
-      为了破解这个局面，我们（以下省略xx字）
+      新章节开放时往往伴有明显的马太效应，即刷的人越多，数据越可靠，越能吸引更多的博士去刷；反之亦然。
+      为了找到隐藏的高效率关卡，我们联合！缺少样本的关卡，我们碎石刷！
+      同时也欢迎大家参与提交数据
     </div>
     <div style="height: 400px;">
       数据表<br>
@@ -17,12 +17,15 @@
       <table class="popup_table">
             <tbody>
               <tr class="popup_table_title">
-                <td class="popup_table_c1" style="width: 55px; width: 65px">关卡名</td>
-                <td class="popup_table_c2" style="width: 65px; width: 75px">主产物</td>
+                <td class="popup_table_c1" style="width: 55px; width: 60px">关卡名</td>
+                <td class="popup_table_c2" style="width: 65px; width: 60px">主产物</td>
+                <td class="popup_table_c2" style="width: 65px; width: 60px">副产物</td>
                 <td class="popup_table_c4" style="width: 50px; width: 60px">样本数</td>
-                <td class="popup_table_c5" style="width: 80px; width: 90px">置信度</td>
-                <td class="popup_table_c6" style="width: 80px; width: 90px">SPM</td>
-                <td class="popup_table_c7" style="width: 70px; width: 80px">关卡效率</td>
+                <td class="popup_table_c5" style="width: 80px; width: 60px">置信度</td>
+                <td class="popup_table_c6" style="width: 80px; width: 40px">SPM</td>
+                <td class="popup_table_c7" style="width: 70px; width: 80px">综合效率</td>
+                <td class="popup_table_c7" style="width: 70px; width: 80px">效率排名</td>
+                <td class="popup_table_c7" style="width: 70px; width: 80px">原始数据</td>
                 <!-- <td class="popup_table_c7" style="width:64px;">小样提升<br>(理论值)</td> -->
               </tr>
               <!-- <tr
@@ -54,8 +57,8 @@
 
     </div>
     <div>
-      每天嫖两次推荐表<br>
-      新玩意
+      [后勤特别许可证]<br>
+      ◆本次活动新增后勤特别许可证：在【惊霆无声】任一可三星代理作战的关卡中，每日前两次作战可使用后勤特别许可证，使用后的当次作战不消耗理智且原常规掉落的报酬改为固定掉落且数量增加至三倍
     </div>
     <div>
       推荐表-占位<br>
@@ -96,15 +99,17 @@ export default {
 
 
 #ep12{
-  background-color: rgb(0, 162, 255);
+  /* background-color: rgb(0, 162, 255); */
   max-width: 720px;
   margin: auto;
   background-image: url(/img/temp/ep12_background.png);
+  /* background-size: 50%; */
+  color:antiquewhite;
 }
 
 #ep12 div{
   margin: 12px;
-  border: 1px solid #000000;
+  border: 1px solid #FFFFFF;
 }
 
 .ep12_support{
