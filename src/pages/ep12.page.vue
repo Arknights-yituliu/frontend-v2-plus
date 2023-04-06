a3c25e9dad5ece1a8595de35427539042e258f09
Zirun-wang <zirunwang.g@gmail.com>
2023-04-06 12:44:04 +0800

    加上了链接 换了底图和logo图，调整了表格以及其他布局，就差数据了，勉强可以发布

--- src/pages/ep12.page.vue
+++ src/pages/ep12.page.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="ep12" style="font-size: 20px;">
     <!-- 占位 -->
-    <div style="height: 192px;"></div>
+    <div style="height: 210px;"></div>
     <!-- 标题 -->
     <div style="text-align: center;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
-      <img src="\public\img\website\ico64.png" style="border-radius: 8px;">
+      <img src="\public\img\website\ico64.png" style="padding: 4px 0px 0px 0px;border-radius: 8px;">
       <div style="font-size: 32px;vertical-align: top;margin:12px;display: inline-block;">
         明日方舟一图流 EP12专题页面
       </div>
     </div>
     <!-- 简单说明 -->
-    <div style="padding:12px;font-style: italic;line-height: 32px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
+    <div style="padding:8px 16px;font-style: italic;line-height: 32px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
       上半页集中展示新章节<b>掉落数据</b><br>
       下半页推荐<b>[后勤特别许可证]</b>的使用优先级
+      <div style="font-size: 14px;">*转载本页内容需注明来源</div>
     </div>
     <!-- 数据补全计划 -->
     <div style="background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
@@ -30,18 +31,18 @@
     </div>
     <!-- 数据补全计划表格 -->
     <div style="height: 400px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
-      <table class="popup_table">
+      <table class="popup_table" style="margin: auto;">
         <tbody>
           <tr class="popup_table_title">
-            <td class="popup_table_c1" style="width: 55px; width: 60px">关卡名</td>
-            <td class="popup_table_c2" style="width: 65px; width: 60px">主产物</td>
-            <td class="popup_table_c2" style="width: 65px; width: 60px">副产物</td>
-            <td class="popup_table_c4" style="width: 50px; width: 60px">样本数</td>
-            <td class="popup_table_c5" style="width: 80px; width: 60px">置信度</td>
-            <td class="popup_table_c6" style="width: 80px; width: 40px">SPM</td>
-            <td class="popup_table_c7" style="width: 70px; width: 80px">综合效率</td>
-            <td class="popup_table_c7" style="width: 70px; width: 80px">效率排名</td>
-            <td class="popup_table_c7" style="width: 70px; width: 80px">原始数据</td>
+            <td class="popup_table_c1" style="width: 55px; width: 72px">关卡名</td>
+            <td class="popup_table_c2" style="width: 65px; width: 72px">产物</td>
+            <!-- <td class="popup_table_c2" style="width: 65px; width: 60px">副产物</td> -->
+            <td class="popup_table_c4" style="width: 50px; width: 72px">样本数</td>
+            <td class="popup_table_c5" style="width: 80px; width: 72px">置信度</td>
+            <td class="popup_table_c6" style="width: 80px; width: 48px">SPM</td>
+            <td class="popup_table_c7" style="width: 70px; width: 96px">综合效率</td>
+            <!-- <td class="popup_table_c7" style="width: 70px; width: 80px">效率排名</td> -->
+            <td class="popup_table_c7" style="width: 70px; width: 96px">原始数据</td>
             <!-- <td class="popup_table_c7" style="width:64px;">小样提升<br>(理论值)</td> -->
           </tr>
           <!-- <tr
@@ -77,8 +78,8 @@
         后勤特别许可证
       </div>
       <div style="vertical-align: top;height: 144px;margin: auto;">
-        <div style="width: 96px;height: 96px;background-color:bisque;display: inline-block;vertical-align: top;margin: 24px 12px 24px 12px;">
-          此处图片
+        <div style="width: 96px;height: 96px;background-color:#888888;display: inline-block;vertical-align: top;margin: 24px 12px 24px 12px;">
+          蹲一个图片
         </div>
         <div style="display: inline-block;max-width: 402px;">
           ◆本次活动新增后勤特别许可证：<br>
@@ -87,47 +88,49 @@
       </div>
     </div>
     <!-- 推荐表-占位 -->
-    <div style="height: 192px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;">
-      机制、数值研究中【来个倒计时，6日20:00归零】<br>
+    <div style="height: 192px;background-color: #ffffff0f;backdrop-filter: blur(8px);margin: 12px;text-align: center;">
+      机制、数值研究中<br>
       数据参考<br>
       【这里放价值表】
     </div>
-    <div>
-      <div class="ep12_support">
+    <div style="display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;">
+      <a class="ep12_support" href="https://space.bilibili.com/21342479">
         <img class="ep12_support_img" src="public\img\website\babel.jpg">
         <div class="ep12_support_text">
           <div class="ep12_support_org">巴别塔攻略组</div>
           <div class="ep12_support_role">数据提供</div>
         </div>
-      </div>
-      <div class="ep12_support">
-        <img class="ep12_support_img" src="public\img\website\penguin_stats_logo.png">
+      </a>
+      <a class="ep12_support" href="https://penguin-stats.cn/">
+        <img class="ep12_support_img" src="public\img\website\ps_logo.png">
         <div class="ep12_support_text">
           <div class="ep12_support_org">企鹅物流数据统计</div>
           <div class="ep12_support_role">数据收集</div>
         </div>
-      </div>
-      <div class="ep12_support">
+      </a>
+      <a class="ep12_support" href="https://yituliu.site/">
         <img class="ep12_support_img" src="public\img\website\ico64.png">
         <div class="ep12_support_text">
           <div class="ep12_support_org">明日方舟一图流</div>
           <div class="ep12_support_role">数据分析</div>
         </div>
-      </div>
-      <div class="ep12_support">
+      </a>
+      <a class="ep12_support" href="https://space.bilibili.com/688411531">
         <img class="ep12_support_img" src="public\img\website\beta_logo_yw.png">
         <div class="ep12_support_text">
           <div class="ep12_support_org">罗德岛基建BETA</div>
           <div class="ep12_support_role">信息发布</div>
         </div>
-      </div>
-      <div class="ep12_support">
+      </a>
+      <!-- <a class="ep12_support">
         <img class="ep12_support_img" src="public\img\website\beta_logo_yw.png">
         <div class="ep12_support_text">
           <div class="ep12_support_org">xxxx</div>
           <div class="ep12_support_role">页面设计</div>
         </div>
-      </div>        
+      </a>         -->
     </div>
   </div>
 </template>
@@ -147,7 +150,9 @@ export default {
 </script>
 
 <style scoped>
-
+a {
+  color: #ffffff;
+}
 
 #ep12{
   /* background-color: rgb(0, 162, 255); */
