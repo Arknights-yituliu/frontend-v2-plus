0bd3504877006f7ca4a0e35102100d56fe18d20a
sakura <1969314158@qq.com>
2023-04-07 19:47:02 +0800

    部分警告提示小问题解决

--- src/pages/ep12.page.server.js
+++ src/pages/ep12.page.server.js
@@ -5,7 +5,7 @@ const http = "https://backend.yituliu.site/";
 // const http = "http://127.0.0.1:10012/";
 
 function get_newChapter() {
-  return axios.get(http + "stage/newChapter?zone=11-");
+  return axios.get(http + "stage/newChapter?zone=12-");
 }
 
 // console.log(http+"stage/newChapter?zone=11-")
