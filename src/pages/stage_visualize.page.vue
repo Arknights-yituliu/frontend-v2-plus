d2a142ffdc610179064bdda3301a8de526acc9da
Zirun-wang <zirunwang.g@gmail.com>
2023-04-07 09:28:39 +0800

    新增关卡数据可视化页面

---
+++ src/pages/stage_visualize.page.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="sv">
+    asdf
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  layout: "poster",
+  data() {
+    return {
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+a {
+  color: #ffffff;
+}
+
+
+</style>
