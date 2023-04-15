1ab508a32cdbaeac99d487550a0777fa001da64e
sakura <1969314158@qq.com>
2023-04-15 18:05:12 +0800

    家具币贷款计算器

---
+++ src/pages/debtCal.page.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+let calResult = ref(0);
+let furniturePartsLoan  = ref(0)  //家具币的贷款
+let furnitureParts  = ref(0)  //库存家具币
+let  carbon_r1 = ref(0)  //库存小碳
+let  carbon_r2 = ref(0)  //库存中碳
+let  carbon_r3 = ref(0)  //库存大碳
+
+function init(){
+    
+
+}
+
+</script>
