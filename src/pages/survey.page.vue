da608e2f3257ba01199484cddb42fc2b196f8d19
sakura <1969314158@qq.com>
2023-04-29 17:36:04 +0800

    干员统计榜单demo

--- src/pages/survey.page.vue
+++ src/pages/survey.page.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <div class="operTable">
+  <div class="main_survey">
+    <div class="survey_title">
+        明日方舟干员持有率统计
+    </div>
+    <div class="survey_tip_box">
+      <div class="survey_tip">
+        <a> 当前样本量</a> <br />
+        114514人次
+      </div>
+      <div class="survey_tip">
+        <a>本表单更新时间</a> <br />
+        2023-11-14
+      </div>
+    </div>
+    <div class="oper_table">
       <div class="oper" v-for="(operData, index) in operServeyData" v-show="operData.rarity > 5">
         <div class="operIndex">No.{{ index + 1 }}</div>
         <img class="image_avatar" :src="'/img/avatar/' + operData.charId + '.png'" alt="" />
