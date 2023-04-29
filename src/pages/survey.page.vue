3e8e6002e5bfe75e0326460eb264f064eb146fa2
sakura <1969314158@qq.com>
2023-04-29 17:17:04 +0800

    干员统计榜单demo

---
+++ src/pages/survey.page.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="operTable">
+      <div class="oper" v-for="(operData, index) in operServeyData" v-show="operData.rarity > 5">
+        <div class="operIndex">No.{{ index + 1 }}</div>
+        <img class="image_avatar" :src="'/img/avatar/' + operData.charId + '.png'" alt="" />
+        <div class="operRate">
+          持有率<br />
+          {{ operData.owningRate.toFixed(1) }}%
+        </div>
+        <div class="operRate">
+          精二率<br />
+          {{ operData.phases2Rate }}%
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import "@/assets/css/survey.css";
+import serveyJson from "@/static/json/survey.json";
+import { ref } from "vue";
+// console.log(serveyJson)
+
+let operServeyData = ref(serveyJson);
+
+function digitLimit(number, digit) {
+  number.toFixed(digit);
+}
+</script>
