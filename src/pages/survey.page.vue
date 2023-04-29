6ff2738732aa917d7727ae6b9d079fb96ea45419
sakura <1969314158@qq.com>
2023-04-29 23:50:52 +0800

    雪碧图实装

--- src/pages/survey.page.vue
+++ src/pages/survey.page.vue
@@ -16,7 +16,8 @@
     <div class="oper_table">
       <div class="oper" v-for="(operData, index) in operServeyData" v-show="operData.rarity > 5">
         <div class="operIndex">No.{{ index + 1 }}</div>
-        <img class="image_avatar" :src="'/img/avatar/' + operData.charId + '.png'" alt="" />
+        <!-- <img class="image_avatar" :src="'/img/avatar/' + operData.charId + '.png'" alt="" /> -->
+        <div :class="getSprite(operData.charId)"></div>
         <div class="operRate">
           持有率<br />
           {{ operData.owningRate.toFixed(1) }}%
@@ -32,13 +33,15 @@
 
 <script setup>
 import "@/assets/css/survey.css";
+import "@/assets/css/sprite_char_6.css";
+import "@/assets/css/survey.css";
 import serveyJson from "@/static/json/survey.json";
 import { ref } from "vue";
 // console.log(serveyJson)
 
 let operServeyData = ref(serveyJson);
 
-function digitLimit(number, digit) {
-  number.toFixed(digit);
+function getSprite(charId,index) {
+ return "image_avatar bg-"+charId;
 }
 </script>
