54a6e16ae4ef8ae36d0d131ef3990b913a09b63f
sakura <1969314158@qq.com>
2023-05-02 00:43:48 +0800

    手动导入初步完成

--- src/api/building.js
+++ src/api/building.js
@@ -1,12 +1,12 @@
 import request from "@/api/request";
 
-const api_name = `/tool`;
+const api_name = `/maa`;
 
 export default {
   //保存排班
   maaBuildingJsonCreated(data, id) {
     return request({
-      url: `${api_name}/building/schedule/save?schedule_id=${id}`,
+      url: `${api_name}/schedule/save?schedule_id=${id}`,
       method: "post",
       data: data,
     });
@@ -14,7 +14,7 @@ export default {
 
   retrieveSchedule(id) {
     return request({
-      url: `${api_name}/building/schedule/retrieve?schedule_id=${id}`,
+      url: `${api_name}/schedule/retrieve?schedule_id=${id}`,
       method: "get",
     });
   },
