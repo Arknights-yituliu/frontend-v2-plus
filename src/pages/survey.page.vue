332f5656313c368fabb99c45fb15723ab2d52cbd
sakura <1969314158@qq.com>
2023-05-01 22:58:51 +0800

    干员统计榜增加手动导入

--- src/pages/survey.page.vue
+++ src/pages/survey.page.vue
@@ -1,12 +1,14 @@
 <template>
+  <div :class="popClass" @click="closePopup()"></div>
+  <div class="survey_popup">
+    <input type="text" v-model="uploadOperBox" class="input_operBox" placeholder="粘贴从MAA干员识别小工具导出的数据" />
+  </div>
+
   <div class="main_survey">
-    <div class="survey_title">
-        明日方舟干员{{surveyType}}统计  
-    </div>
+    <div class="survey_title">明日方舟干员{{ surveyType }}统计</div>
     <div class="survey_button_box">
-    <div>
-        <button class="survey_button" @click="switchSurvey()">切换为{{'精二率'==surveyType?'持有率':'精二率'}}统计</button>
-      </div>
+      <button class="survey_button" @click="switchSurvey()">切换为{{ "精二率" == surveyType ? "持有率" : "精二率" }}统计</button>
+      <button class="survey_button" @click="inputOperBox()">手动导入干员信息</button>
     </div>
 
     <div class="survey_tip_box">
@@ -19,7 +21,7 @@
         2023-11-14
       </div>
     </div>
-   
+
     <div class="oper_table">
       <div class="oper" v-for="(operData, index) in operServeyData" v-show="operData.rarity > 5">
         <div class="operIndex">No.{{ index + 1 }}</div>
@@ -43,11 +45,15 @@ import "@/assets/css/survey.css";
 import "@/assets/css/sprite_char_6.css";
 import "@/assets/css/survey.css";
 import serveyJson from "@/static/json/survey.json";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 // console.log(serveyJson)
 
 let operServeyData = ref(serveyJson);
 let surveyType = ref("持有率");
+let popClass = ref("survey_popup_mask");
+
+let uploadOperBox = ref("");
+let operBoxJson = ref("");
 
 function getSprite(charId, index) {
   return "image_avatar bg-" + charId;
@@ -60,4 +66,20 @@ function switchSurvey() {
 
   return (surveyType.value = "持有率");
 }
+
+function inputOperBox() {
+  popClass.value = "survey_popup_mask";
+}
+
+function closePopup() {
+  popClass.value = "";
+}
+
+watch(uploadOperBox, (new_value) => {
+  let obj = {};
+  obj = JSON.parse(new_value);
+  console.log(obj[0]);
+  uploadOperBox.value = JSON.stringify(obj, null, 4);
+  // console.log(uploadOperBox.value)
+});
 </script>
