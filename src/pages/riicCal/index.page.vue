e7a678ebfc7cf21e6f76bc72a76113382a2b7439
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-06-12 21:00:53 +0800

    继续优化riicCal index按钮

--- src/pages/riicCal/index.page.vue
+++ src/pages/riicCal/index.page.vue
@@ -1,87 +1,89 @@
 <template>
   <div class="riic-bg">
-    <a class="riic-btn">
+    <div class="riic-btn">
       <picture class="riic-icon">
         <source srcset="/image/website/maa.webp" type="image/webp" />
         <source srcset="/image/website/maa.png" type="image/png" />
         <img src="/image/website/maa.png" />
       </picture>
-      <div class="riic-info">
-        <div class="riic-guide">
-          Maa及排班表生成的使用讲解：
-          <span>BV1De411L7C9</span>
-        </div>
-        <div class="riic-title">Maa Assistant Arknights</div>
-        <div class="riic-intro">每日定时进行三次换班</div>
+      <div class="riic-title">Maa Assistant Arknights</div>
+      <div class="riic-intro">每日定时进行三次换班</div>
+      <div class="riic-guide">
+        Maa及排班表生成的使用讲解：
+        <span>BV1De411L7C9</span>
       </div>
-    </a>
-    <a class="riic-btn">
+    </div>
+    <div class="riic-btn">
       <picture class="riic-icon">
         <source srcset="/image/website/mower.webp" type="image/webp" />
         <source srcset="/image/website/mower.png" type="image/png" />
         <img src="/public/image/website/mower.png" />
       </picture>
-      <div class="riic-info">
-        <div class="riic-guide">
-          跑单玩法及Mower工具讲解：
-          <span>BV1KT411s7Ar</span>
-        </div>
-        <div class="riic-title">Arknights Mower</div>
-        <div class="riic-intro">动态换班，使用但书与龙舌兰跑单</div>
+      <div class="riic-title">Arknights Mower</div>
+      <div class="riic-intro">动态换班，使用但书与龙舌兰跑单</div>
+      <div class="riic-guide">
+        跑单玩法及Mower工具讲解：
+        <span>BV1KT411s7Ar</span>
       </div>
-    </a>
+    </div>
   </div>
 </template>
 
 <style>
 .riic-bg {
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  height: calc(100vh - 94px);
+  /* min-height: calc(100vh - 156px); */
+  padding: 30px 0;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   gap: 30px;
   justify-content: center;
   align-items: center;
+  align-content: center;
+  flex-wrap: wrap;
 }
 
 .riic-btn {
-  width: 500px;
-  display: block;
+  width: 460px;
+  height: 382px;
   border-radius: 14px;
   box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.08), 0 3px 6px 0 rgba(0, 0, 0, 0.06), 0 5px 12px 4px rgba(0, 0, 0, 0.04);
-  height: 138px;
   display: flex;
+  flex-direction: column;
   align-items: center;
+  justify-content: center;
+  gap: 8px;
+  cursor: pointer;
+  box-sizing: border-box;
+  transition: background-color 0.2s;
+}
+
+.riic-btn:hover {
+  background-color: #18a058;
+  color: white;
 }
 
 .riic-icon {
-  margin: 0 18px 0 18px;
+  margin-bottom: 8px;
 }
 
 .riic-icon source,
 .riic-icon img {
-  width: 100px;
-  height: 100px;
-}
-
-.riic-info {
-  display: flex;
-  flex-direction: column;
-  height: 90px;
-  justify-content: space-between;
+  width: 200px;
+  height: 200px;
 }
 
 .riic-guide {
-  font-size: 16px;
+  font-size: 18px;
 }
 
 .riic-title {
-  font-size: 24px;
+  font-size: 28px;
   font-weight: bold;
 }
 
 .riic-intro {
-  font-size: 20px;
+  font-size: 22px;
 }
 </style>
 
