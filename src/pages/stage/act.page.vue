00da5d8c8f86d0bd3cdd93789cd0a27ba339ad83
sakura <1969314158@qq.com>
2023-06-24 09:41:32 +0800

    部分布局小修改

--- src/pages/stage/act.page.vue
+++ src/pages/stage/act.page.vue
@@ -1,14 +1,22 @@
 <template>
+  <div class="act_bar">
+    <div class="act_title"></div>
+    <div v-for="item in itemArr" class="item_wrap_title">
+      <div :class="getSpriteImg(item.id)"></div>
+    </div>
+  </div>
   <div v-for="act in closedStageList">
-    <div class="act_bar" v-show="act[0].zoneName.indexOf('复刻')==-1">
-      <div>
+    <div class="act_bar" v-show="act[0].zoneName.indexOf('复刻') == -1">
+      <div class="act_title">
         <div class="zoneName_wrap">{{ act[0].zoneName }}</div>
-        <div class="openTime_wrap">{{ act[0].openTime }}</div>
+        <div class="openTime_wrap">{{ act[0].openTime.slice(0.8) }}</div>
       </div>
-      <div v-for="itemName in itemArr" class="item_wrap">
-        <div v-for="stage in act" v-show="stage.itemType == itemName">
+      <div v-for="item in itemArr" class="item_wrap">
+        <div v-for="stage in act" v-show="stage.itemType == item.name">
           <div :class="getSpriteImg(stage.itemId)"></div>
+          <div>{{ stage.stageEfficiency.toFixed(2) }}%</div>
         </div>
+       
       </div>
     </div>
   </div>
@@ -27,22 +35,22 @@ stageApi.findClosedStage(0.625).then((response) => {
 });
 
 let itemArr = [
-  "RMA70-12",
-  "全新装置",
-  "扭转醇",
-  "轻锰矿",
-  "酮凝集组",
-  "研磨石",
-  "半自然溶剂",
-  "异铁组",
-  "凝胶",
-  "糖组",
-  "转质盐组",
-  "聚酸酯组",
-  "炽合金",
-  "固源岩组",
-  "晶体元件",
-  "化合切削液",
+  { name: "RMA70-12", id: "30103" },
+  { name: "全新装置", id: "30063" },
+  { name: "扭转醇", id: "30073" },
+  { name: "轻锰矿", id: "30083" },
+  { name: "研磨石", id: "30093" },
+  { name: "酮凝集组", id: "30053" },
+  { name: "糖组", id: "30023" },
+  { name: "凝胶", id: "30043" },
+  { name: "异铁组", id: "30073" },
+  { name: "半自然溶剂", id: "31043" },
+  { name: "聚酸酯组", id: "30033" },
+  { name: "转质盐组", id: "31063" },
+  { name: "炽合金", id: "31023" },
+  { name: "固源岩组", id: "31033" },
+  { name: "晶体元件", id: "30073" },
+  { name: "化合切削液", id: "31053" },
 ];
 
 function getSpriteImg(id, index) {
@@ -53,7 +61,10 @@ function getSpriteImg(id, index) {
 <style>
 .act_bar {
   display: flex;
-  
+}
+
+.act_title {
+  min-width: 200px;
 }
 
 .zoneName_wrap {
@@ -66,19 +77,25 @@ function getSpriteImg(id, index) {
   margin-top: 20px;
 }
 
-.openTime_wrap{
+.openTime_wrap {
   min-width: 200px;
   height: 20px;
   line-height: 20px;
   text-align: center;
   font-size: 18px;
   /* font-weight: 900; */
-  margin-bottom: 10px;
+  /* margin-bottom: 10px; */
+}
+
+.item_wrap_title {
+  min-width: 60px;
+  border: 1px rgb(0, 0, 0) solid;
 }
 
 .item_wrap {
-  min-width: 70px;
-  height: 70px;
-  border: 1px red solid;
+  min-width: 60px;
+  height: 80px;
+  border: 1px rgb(0, 0, 0) solid;
+  text-align: center;
 }
 </style>
