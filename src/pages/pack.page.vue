d910df786007fa43021f596c12d67227afe63a7a
sakura <1969314158@qq.com>
2023-06-30 22:38:39 +0800

    礼包性价比：礼包图片链接修改

--- src/pages/pack.page.vue
+++ src/pages/pack.page.vue
@@ -631,8 +631,10 @@ export default {
       // if(true ===this.showFlag) return '';
       // console.log(true ===this.showFlag);
       if (type === "limited") {
-        return "background:url(/image/packs/limited/" + img + ".png) 0% 0% / cover no-repeat,#444444;";
-      } else return "background:url(/image/packs/small/" + img + ".jpg) 0% 0% / cover no-repeat,#444444;";
+        return "background:url(/image/packs/limited/" + img + ".jpg) 0% 0% / cover no-repeat,#444444;";
+      } else {
+        return "background:url(/image/packs/small/" + img + ".jpg) 0% 0% / cover no-repeat,#444444;";
+      }
     },
     getContentId(id, type) {
       return type + "_" + id;
