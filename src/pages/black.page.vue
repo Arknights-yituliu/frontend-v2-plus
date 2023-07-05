0701ef29c9eebcf634749514e83f0ed6a00bb596
Zirun-wang <zirunwang.g@gmail.com>
2023-07-06 06:08:56 +0800

    完善各个导航页面

---
+++ src/pages/black.page.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="black_black">
+    <div class="black-card">
+      <picture class="riic-icon">
+        <source srcset="https://avatars.githubusercontent.com/u/97677443?s=200" type="image/webp" />
+        <source srcset="https://avatars.githubusercontent.com/u/97677443?s=200" type="image/png" />
+        <img src="https://avatars.githubusercontent.com/u/97677443?s=200" />
+      </picture>
+      <div class="riic-title">Maa Assistant Arknights</div>
+      <div class="riic-intro">每日定时进行三次换班</div>
+      <div class="riic-guide">
+        Maa及排班表生成的使用讲解：
+        <span>BV1De411L7C9</span>
+      </div>
+    </div>
+  </div>
+  <div class="black_white">
+    <div class="white-card">
+      <picture class="riic-icon">
+        <source srcset="https://avatars.githubusercontent.com/u/97677443?s=200" type="image/webp" />
+        <source srcset="https://avatars.githubusercontent.com/u/97677443?s=200" type="image/png" />
+        <img src="https://avatars.githubusercontent.com/u/97677443?s=200" />
+      </picture>
+      <div class="riic-title">Maa Assistant Arknights</div>
+      <div class="riic-intro">每日定时进行三次换班</div>
+      <div class="riic-guide">
+        Maa及排班表生成的使用讲解：
+        <span>BV1De411L7C9</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.black_white{
+  
+}
+</style>
+
+<script>
+export const documentProps = {
+  title: "一图流 - 排班生成器",
+  description: "明日方舟一图流，排班生成器，基建排班",
+};
+</script>
