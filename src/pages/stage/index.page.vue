85ed352becaa29043ca09352ced520f726cd0ad3
Zirun-wang <zirunwang.g@gmail.com>
2023-08-01 06:01:31 +0800

    完善导航信息

--- src/pages/stage/index.page.vue
+++ src/pages/stage/index.page.vue
@@ -4,13 +4,13 @@ import NaviCards from "@/components/NaviCards.vue";
 const navi_data = [
   {
     title: "刷图推荐",
-    desc: ["我该去哪刷图？", "活动图效率怎么样？", "XX图适合不适合搓玉？", "XX材料什么时候UP过？"],
+    desc: ["我该去哪刷图？", "活动图效率怎么样？", "XX图能不能搓玉？", "XX材料何时UP过？"],
     icon: "/image/icon/无框_全新装置.png",
     link: "/",
   },
-  { title: "商店性价比", desc: ["绿票、橙票买哪个比较赚？", "活动商店优先换哪些材料？"], icon: "/image/icon/无框_四凭证.png", link: "/stage/store" },
+  { title: "商店性价比", desc: ["绿票、橙票买哪个？", "活动商店优先换什么？"], icon: "/image/icon/无框_四凭证.png", link: "/stage/store" },
   { title: "礼包性价比", desc: ["礼包综合性价比", "仅计抽卡的性价比"], icon: "/image/icon/无框_龙门币.png", link: "/stage/pack" },
-  { title: "物品价值表", desc: ["查看物品的价值", "导出为JSON或Excel", "以作研究或二次开发"], icon: "/image/icon/无框_理智.png", link: "/stage/value" },
+  { title: "物品价值表", desc: ["查看物品的价值", "导出JSON或Excel", "研究或二次开发"], icon: "/image/icon/无框_理智.png", link: "/stage/value" },
 ];
 </script>
 
