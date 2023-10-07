ef2da27cc11dae62e697b094e2db765003504302
sakura <1969314158@qq.com>
2023-10-07 21:59:33 +0800

    修改EP13专题页面部分内容

--- src/pages/material/ep13.page.vue
+++ src/pages/material/ep13.page.vue
@@ -69,20 +69,20 @@
       </table>
     </div>
     <!-- 后勤特别许可证 -->
-    <div style="background-color: #ffffff0f; backdrop-filter: blur(8px); margin: 12px">
-      <div style="text-align: center; font-size: 24px; font-weight: 600; padding: 12px 0px">后勤特别许可证</div>
-      <div style="vertical-align: top; margin: auto">
-        <div
-          style="width: 96px; height: 96px; background: url('/image/temp/permit.webp'); display: inline-block; vertical-align: top; margin: 18px 12px 24px 12px"
-        ></div>
-        <div style="display: inline-block; max-width: 75%; line-height: 32px; padding-bottom: 8px">
-          <div>◆本次活动新增后勤特别许可证：</div>
-          <div>
-            在【惊霆无声】任一可三星代理作战的关卡中，每日前两次作战可使用后勤特别许可证，使用后的当次作战不消耗理智且原常规掉落的报酬改为固定掉落且数量增加至三倍
-          </div>
-        </div>
-      </div>
-    </div>
+<!--    <div style="background-color: #ffffff0f; backdrop-filter: blur(8px); margin: 12px">-->
+<!--      <div style="text-align: center; font-size: 24px; font-weight: 600; padding: 12px 0px">后勤特别许可证</div>-->
+<!--      <div style="vertical-align: top; margin: auto">-->
+<!--        <div-->
+<!--          style="width: 96px; height: 96px; background: url('/image/temp/permit.webp'); display: inline-block; vertical-align: top; margin: 18px 12px 24px 12px"-->
+<!--        ></div>-->
+<!--        <div style="display: inline-block; max-width: 75%; line-height: 32px; padding-bottom: 8px">-->
+<!--          <div>◆本次活动新增后勤特别许可证：</div>-->
+<!--          <div>-->
+<!--            在【惊霆无声】任一可三星代理作战的关卡中，每日前两次作战可使用后勤特别许可证，使用后的当次作战不消耗理智且原常规掉落的报酬改为固定掉落且数量增加至三倍-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
     <!-- 推荐表-占位 -->
 
     <div style="background-color: #ffffff0f; backdrop-filter: blur(8px); margin: 12px; text-align: center">
