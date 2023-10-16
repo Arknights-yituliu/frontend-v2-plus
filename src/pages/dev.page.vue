545617181a67e894cc4464288046b1d45e0e8fd3
Zirun-wang <zirunwang.g@gmail.com>
2023-10-16 20:02:58 +0800

    试着新增一个页面

---
+++ src/pages/dev.page.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="indexDiv" >
+    <newChapter />
+
+    <dev />
+
+    <foot-component />
+
+
+  </div>
+</template>
+
+<style scoped>
+#indexDiv {
+  margin: auto;
+  color: var(--index-div-fg);
+  background-color: var(--index-div-bg);
+}
+
+
+</style>
+
+<script setup>
+import dev from "@/components/dev.vue";
+
+import FootComponent from "@/components/FootComponent.vue";
+import newChapter from '/src/components/newChapter.vue'
+
+
+import { onMounted } from "vue";
+
+onMounted(() => {});
+</script>
+
+<script>
+export const documentProps = {
+  title: "明日方舟一图流",
+  description: "明日方舟材料掉落地图效率，采购中心性价比，活动商店性价比，物品价值一览",
+};
+</script>
