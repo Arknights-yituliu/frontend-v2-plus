6aa106466a24d74d1d50e88e65c632c0afee4d9e
sakura <1969314158@qq.com>
2023-11-15 01:29:36 +0800

    攒抽计算器更新

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,4 +1,4 @@
-const http = "https://backend.yituliu.site/";
-// const http = "http://127.0.0.1:10010/";
+// const http = "https://backend.yituliu.site/";
+const http = "http://127.0.0.1:10010/";
 
 export { http };
