2a3c01a36513b839c46f62f19153880c67837c1e
sakura <1969314158@qq.com>
2024-01-04 16:03:54 +0800

    增加首页b站发布号转跳框

--- src/pages/utils/utils.js
+++ src/pages/utils/utils.js
@@ -37,21 +37,9 @@ function throttle(func,begin, delay = 500,message = '操作过于频繁') {
     };
 }
 
-function fileRead(file){
-    return new Promise((resolve,reject)=>{
-        const reader = new FileReader()
-        reader.onload = () =>{
-            resolve(reader.result)
-        }
 
-        reader.onerror = () =>{
-            reject(reader.error)
-        }
-        reader.readAsText(file,'UTF-8')
-    })
-}
 
 
 export {
-    debounce, throttle, fileRead
+    debounce, throttle
 }
