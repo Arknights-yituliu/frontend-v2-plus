8f47fd88b4aa7ab14aae390488013da36b35ead4
sakura <1969314158@qq.com>
2024-01-08 18:20:03 +0800

    新增日志页面

---
+++ src/pages/about/log.page.vue
@@ -0,0 +1,32 @@
+<script setup>
+import logTable from '/src/static/json/log.json'
+
+</script>
+
+<template>
+
+  <div class="log-list">
+    <h1>网站维护日志</h1>
+    <div class="log-item" v-for="({date,logs},index) in logTable" :key="index">
+      <h2>{{ date }}</h2>
+      <ul>
+        <li class="log-content" v-for="(log,index) in logs" :key="index">{{`【${log.tag}】`}}&emsp;{{log.text}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.log-list{
+  width: 90%;
+  margin: auto;
+}
+
+.log-item {
+
+}
+
+.log-content{
+  padding: 4px;
+}
+</style>
