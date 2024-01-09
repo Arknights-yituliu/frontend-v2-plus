a0a6c98af93c0fb671d67f1a71e0aa0cee6e5ae0
sakura <1969314158@qq.com>
2024-01-09 22:32:07 +0800

    日志

--- src/pages/about/log.page.vue
+++ src/pages/about/log.page.vue
@@ -10,23 +10,47 @@ import logTable from '/src/static/json/log.json'
     <div class="log-item" v-for="({date,logs},index) in logTable" :key="index">
       <h2>{{ date }}</h2>
       <ul>
-        <li class="log-content" v-for="(log,index) in logs" :key="index">{{`【${log.tag}】`}}&emsp;{{log.text}}&emsp;@{{log.author}}</li>
+        <li class="log-content" v-for="(log,index) in logs" :key="index">
+          <span >{{ `【${log.tag}】` }}</span>
+          <span >{{ log.text }}</span>
+          <span >@{{ log.author }}</span>
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
 <style scoped>
-.log-list{
+.log-list {
   width: 90%;
   margin: auto;
+}
+
+.log-content {
+  padding: 4px;
+  display: flex;
+}
+
+.log-content span{
+  padding:0 8px;
 }
 
 .log-item {
 
 }
 
-.log-content{
-  padding: 4px;
+.log-tag {
+  width: 200px;
 }
+
+.log-text {
+  width: 200px;
+}
+
+.log-author {
+  width: 200px;
+}
+
+
+
 </style>
