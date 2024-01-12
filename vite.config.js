20a4e6fc762de729b64b2be870e4eb070f58cbd7
sakura <1969314158@qq.com>
2024-01-12 19:13:06 +0800

    将部分第三方库改为本地加载

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,59 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import {fileURLToPath, URL} from "node:url";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import ssr from "vite-plugin-ssr/plugin";
-import { visualizer } from "rollup-plugin-visualizer";
+import {visualizer} from "rollup-plugin-visualizer";
 import externalGlobals from "rollup-plugin-external-globals";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  const config = {
-    plugins: [vue(), ssr()],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-    build: {
-      target: "es2015",
-      cssTarget: "chrome61",
-      rollupOptions: {},
-    },
-  };
-  if (command == "build" && mode == "production" && !ssrBuild) {
-    config.plugins.push(visualizer());
-    config.build.rollupOptions = {
-      external: ["vue", "element-plus", "js-cookie", "echarts", "@element-plus/icons-vue", "axios", "mdui"],
-      plugins: [
-        externalGlobals({
-          vue: "Vue",
-          "element-plus": "ElementPlus",
-          "js-cookie": "Cookies",
-          echarts: "echarts",
-          "@element-plus/icons-vue": "ElementPlusIconsVue",
-          axios: "axios",
-          mdui: "mdui",
-        }),
-      ],
-      output: {
-        manualChunks: (id, { getModuleInfo }) => {
-          if (getModuleInfo(id).isEntry) {
-            if (id.includes("src/renderer")) {
-              return "bundle";
-            }
-          } else if (!id.includes("/src/components/")) {
-            return "bundle";
-          }
+export default defineConfig(({command, mode, ssrBuild}) => {
+    const config = {
+        plugins: [vue(), ssr()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
         },
-      },
+        build: {
+            target: "es2015",
+            cssTarget: "chrome61",
+            rollupOptions: {},
+        },
     };
-  }
-  return config;
+    if (command == "build" && mode == "production" && !ssrBuild) {
+        config.plugins.push(visualizer());
+        config.build.rollupOptions = {
+            // external: ["vue", "element-plus", "js-cookie", "echarts", "@element-plus/icons-vue", "axios", "mdui"],
+            // plugins: [
+            //   externalGlobals({
+            //     vue: "Vue",
+            //     "element-plus": "ElementPlus",
+            //     "js-cookie": "Cookies",
+            //     echarts: "echarts",
+            //     "@element-plus/icons-vue": "ElementPlusIconsVue",
+            //     axios: "axios",
+            //     mdui: "mdui",
+            //   }),
+            // ],
+            external: [ "echarts",  "mdui"],
+            plugins: [
+                externalGlobals({
+                    echarts: "echarts",
+                    mdui: "mdui",
+                }),
+            ],
+            output: {
+                manualChunks: (id, {getModuleInfo}) => {
+                    if (getModuleInfo(id).isEntry) {
+                        if (id.includes("src/renderer")) {
+                            return "bundle";
+                        }
+                    } else if (!id.includes("/src/components/")) {
+                        return "bundle";
+                    }
+                },
+            },
+        };
+    }
+    return config;
 });
