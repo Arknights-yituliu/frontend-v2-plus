47fff64ee399f6cb8112dd57baa0674459cc5405
yamazakura <1969314158@qq.com>
2024-01-20 16:33:00 +0800

    【肉鸽计算器】更新exe文件

--- src/pages/tools/rogueCal.page.vue
+++ src/pages/tools/rogueCal.page.vue
@@ -530,7 +530,7 @@ export default {
       }
     },
     downloadCalcExe(){
-       window.open('/RogueCalcV1/demo.exe')
+       window.open('/RogueCalcV1/formalCalc.exe')
     }
   },
   mounted() {
