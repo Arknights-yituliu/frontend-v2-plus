06bbf587c4facf9f11f03d992766354d12e53d30
yamazakura <1969314158@qq.com>
2024-01-21 00:16:33 +0800

    【基建技能一览】新增基建技能一览页面

---
+++ src/custom/collapse-item-v2.vue
@@ -0,0 +1,84 @@
+<template>
+
+  <div :id="collapseWrapId" class="c_collapse_item_wrap">
+    <div :id="collapseItemId" class="c_collapse_item">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {watch, onMounted} from "vue";
+
+const emit = defineEmits(["update:visible"]);
+const props = defineProps(["modelValue", "visible", "name", 'style']);
+
+let collapseWrapId = "collapse-wrap-" + props.name
+let collapseItemId = "collapse-item-" + props.name
+
+onMounted(() => {
+  if (props.visible) {
+    let element = document.getElementById(collapseItemId);
+    let height = element.offsetHeight;
+    let element_wrap = document.getElementById(collapseWrapId);
+    element_wrap.style.height = height + "px";
+    setTimeout(() => {
+      if (element_wrap.offsetHeight > 1) {
+        element_wrap.style.height = 'auto'
+      }
+    }, 300);
+  }
+})
+
+
+function collapse(flag) {
+
+  let wrapElement = document.getElementById(collapseWrapId);
+
+  let start = new Date()
+
+  let contentElement = document.getElementById(collapseItemId);
+  let height = contentElement.clientHeight;
+  console.log(height)
+  const animate = () => {
+
+    const elapsed = Date.now() - start;
+    const progress = Math.min(elapsed / 100, 1)
+    const heightStep = flag ? height * progress : height * (1 - progress)
+
+    wrapElement.style.height = `${heightStep}px`
+
+    console.log(progress, '高度步进', heightStep)
+    if (progress < 1) {
+      requestAnimationFrame(animate)
+    } else {
+
+      wrapElement.style.height = flag?`${height}px`:'0px'
+    }
+
+  }
+
+  animate()
+}
+
+watch(() => props.visible, (new_val) => {
+  collapse(new_val)
+  emit("update:visible", new_val);
+})
+
+</script>
+
+<style scoped>
+
+.c_collapse_item_wrap {
+  height: 0;
+  overflow: hidden;
+  transition: height 0.2s linear;
+}
+
+.c_collapse_item {
+  padding: 1px;
+}
+
+
+</style>
--- src/pages/tools/buildingSkill.page.client.vue
+++ src/pages/tools/buildingSkill.page.client.vue
@@ -1,60 +1,249 @@
 <script setup>
-import '/src/assets/css/tool/schedule.css'
 import '/src/assets/css/tool/building_skill.css'
 
 import {operatorFilterConditionTable} from "/src/pages/tools/skillFilter";
 import building_table from '/src/static/json/build/building_table.json'
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
+import {debounce} from "../../utils/debounce";
+
+let buildingTable = {}
+for (const operator of building_table) {
+  if (!buildingTable[operator.charId]) {
+    buildingTable[operator.charId] = []
+  }
+  buildingTable[operator.charId].push(operator)
+}
 
 const COLOR = {BLUE: 'blue'}
 
-let selectBtnKey = ref({})
-let selectedOperatorList = ref({})
+let selectBtnKey = ref('')
+let filterOperatorList = ref([])
+
 
 function filterBtnStatus(key, label) {
   return selectBtnKey.value === `${key}+${label}`
 }
+
+let filterCondition = ref({
+  func: () => {
+  }
+})
 
 /**
  * 筛选干员
  * @param condition
  * @param key 选项的分类名
  */
-function filterOperatorList(condition, key) {
-  selectBtnKey.value = `${key}+${condition.label}`
-  selectedOperatorList.value = {}
-  for (const operator of building_table) {
-    if (condition.func(operator)){
-      if(!selectedOperatorList.value[operator.charId]){
-        selectedOperatorList.value[operator.charId] = []
-      }
-      console.log(selectedOperatorList.value[operator.charId])
-      selectedOperatorList.value[operator.charId].push(operator)
-    }
+function filterOperatorByTag(condition, key) {
 
+  //清空干员列表
+  filterOperatorList.value = []
+
+  const btnKey = `${key}+${condition.label}`
+  //判断按钮是否已经选中，已经选中则清空暂存的筛选函数和按钮key，撤销选中状态
+  if (selectBtnKey.value === btnKey) {
+    selectBtnKey.value = ''
+    filterCondition.value = ''
+  } else {
+    //暂存筛选函数和按钮key
+    selectBtnKey.value = btnKey
+    filterCondition.value = condition;
   }
+
+
+  //筛选干员
+  commonFilterOperator()
 }
 
+
+//干员搜索输入框
+let searchInputText = ref('')
+/**
+ * 根据输入的名称和技能描述搜索干员
+ */
+const searchOperatorDebounce = debounce(() => {
+  //清空干员列表
+  filterOperatorList.value = []
+  //筛选干员
+  commonFilterOperator()
+}, 500)
+
+
+let hideIrrelevantSkillsFlag = ref(false)
+
+let skillRowCount = ref({})
+
+function hideIrrelevantSkills() {
+  hideIrrelevantSkillsFlag.value = !hideIrrelevantSkillsFlag.value
+  searchOperatorDebounce()
+}
+
+/**
+ * 通用的筛选干员逻辑
+ */
+function commonFilterOperator() {
+
+  for (const charId in buildingTable) {
+    const skills = buildingTable[charId]
+
+    let rowCount = 0;
+    let rowIndex = filterOperatorList.value.length
+    if (!skillRowCount.value[charId]) {
+      skillRowCount.value[charId] = {}
+    }
+    for (const skill of skills) {
+
+      // 当按钮key有值时通过暂存的筛选函数进行筛选
+      if (selectBtnKey.value && !filterCondition.value.func(skill)) {
+        continue;
+      }
+      //通过输入关键词筛选
+      if (searchInputText.value && !operatorHasKeyword(skill)) {
+        continue;
+      }
+
+
+      if (!hideIrrelevantSkillsFlag.value) {
+        for (const operator1 of skills) {
+          filterOperatorList.value.push(operator1)
+          rowCount++
+        }
+        break
+      }
+
+      filterOperatorList.value.push(skill)
+      rowCount++
+    }
+
+    skillRowCount.value[charId] = {index: rowIndex, rowCount: rowCount}
+  }
+
+
+}
+
+function mergeRow(id, index) {
+
+  return skillRowCount.value[id] ? skillRowCount.value[id] : {index: index, rowCount: 1}
+}
+
+
+/**
+ * 判断干员的干员名称、技能名称、技能描述是否包含输入的搜索关键词
+ * @param operator 干员信息
+ * @return {boolean} 是否包含关键词
+ */
+function operatorHasKeyword(operator) {
+  return operator.name.indexOf(searchInputText.value) > -1 ||
+      operator.description.indexOf(searchInputText.value) > -1 ||
+      operator.buffName.indexOf(searchInputText.value) > -1
+}
+
+function getUnlock(phase, level) {
+  if (phase > 0) {
+    return `精${phase}`
+  }
+
+  if (level > 0) {
+    return `30级`
+  }
+
+  return '无'
+}
+
+const roomTypeMenu = [
+  {label: "贸易站", value: "trading"},
+  {label: "制造站", value: "manufacture"},
+  {label: "办公室", value: "hire"},
+  {label: "加工站", value: "workshop"},
+  {label: "宿舍", value: "dormitory"},
+  {label: "控制中枢", value: "control"},
+  {label: "发电站", value: "power"},
+  {label: "会客室", value: "meeting"},
+  {label: "训练室", value: "training"}
+]
+
+let roomTypeDict = {}
+for (const roomType of roomTypeMenu) {
+  roomTypeDict[roomType.value] = roomType.label
+}
+
+/**
+ * 获得房间名称
+ * @param type 设施类型
+ * @return {string} 房间名称
+ */
+function getRoomLabel(type) {
+  return roomTypeDict[type]
+}
+
+function getAvatar(id) {
+  return `bg-${id} building-operator-avatar`
+}
+
+onMounted(() => {
+  searchOperatorDebounce()
+})
+
 </script>
+
 <template>
 
-  <div class="filter-condition-box">
-    <div class="condition-bar" v-for="(room,key) in operatorFilterConditionTable" v-show="room.display" :key="key">
+  <div class="b-filter-condition-box">
+    <div class="b-condition-bar" v-for="(room,key) in operatorFilterConditionTable" v-show="room.display" :key="key">
       <span :style="`color:${room.color}`">{{ room.name }}</span>
-      <c-button v-for="(condition,index) in room.conditions" :key="index"
+      <c-button v-for="(condition,index) in room.conditions" :key="index" style="margin: 2px"
                 :color="COLOR.BLUE" :status="filterBtnStatus(key,condition.label)"
-                @click="filterOperatorList(condition,key)">
+                @click="filterOperatorByTag(condition,key)">
         {{ condition.label }}
       </c-button>
     </div>
   </div>
 
-  <div class="skill-card" v-for="(skills,index) in selectedOperatorList" :key="index">
-    <div class="building-operator-name">{{skills[0].name}}</div>
-    <div v-for="(skill,index) in skills" :key="index">
-       {{skill.buffName}}
-    </div>
+  <div class="b-building-search-wrap">
+    <span style="padding:0 16px">搜索干员</span>
+    <input class="input-base" @input="searchOperatorDebounce()" v-model="searchInputText">
+    <c-button :status="hideIrrelevantSkillsFlag" :color="COLOR.ORANGE"
+              style="margin-left: 12px" @click="hideIrrelevantSkills">隐藏无关技能
+    </c-button>
+    <!--  <span-->
+    <!--      style="font-style: italic;font-size: 14px">（搜索栏可输入干员名、技能描述，可与上面的预设TAG按钮共同生效筛选,再次点击按钮可取消按钮)</span>-->
+
   </div>
+
+  <table class="building-skill-table">
+    <tbody>
+    <tr style="line-height: 42px;">
+      <td style="width:120px">干员</td>
+      <td style="width:50px">解锁</td>
+      <td style="width:70px">设施</td>
+      <td style="width:100px">技能</td>
+      <td>描述</td>
+    </tr>
+
+    <tr v-for="(operator,index) in filterOperatorList" :key="index">
+      <td :rowspan="mergeRow(operator.charId,index).rowCount"
+          v-if="mergeRow(operator.charId,index).index===index">
+        <div class="building-operator-avatar-wrap">
+          <div class="building-operator-avatar-">
+            <div :class="getAvatar(operator.charId)"></div>
+            <span>{{ operator.name }}</span>
+          </div>
+        </div>
+      </td>
+      <td>{{ getUnlock(operator.phase, operator.level) }}</td>
+      <td>{{ getRoomLabel(operator.roomType) }}</td>
+      <td>
+        <span :style="`background:${operator.buffColor};color:${operator.textColor}`" class="b-building-skill-name">
+          {{ operator.buffName }}
+        </span>
+      </td>
+      <td>
+        <span style="line-height: 36px" v-html="operator.description"></span>
+      </td>
+    </tr>
+
+    </tbody>
+  </table>
 
 
 </template>
