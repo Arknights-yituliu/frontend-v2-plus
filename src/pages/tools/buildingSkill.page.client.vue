8763f589b5bada4293474335ed54b8ea3a016882
MilkWind <2250957906@qq.com>
2024-01-21 23:49:52 +0800

    【基建技能一览】调整单技能展示时的单元格高度
    Signed-off-by: MilkWind <2250957906@qq.com>

--- src/pages/tools/buildingSkill.page.client.vue
+++ src/pages/tools/buildingSkill.page.client.vue
@@ -233,13 +233,14 @@ onMounted(() => {
         </td>
         <td>{{ getUnlock(operator.phase, operator.level) }}</td>
         <td>{{ getRoomLabel(operator.roomType) }}</td>
-        <td style="width: 140px">
-        <span :style="`background:${operator.buffColor};color:${operator.textColor}`" class="b-building-skill-name">
+        <td>
+        <span :style="`background:${operator.buffColor};color:${operator.textColor}`"
+              class="b-building-skill-name">
           {{ operator.buffName }}
         </span>
         </td>
-        <td style="padding-inline: 30px">
-          <span style="line-height: 40px;" v-html="operator.description"></span>
+        <td style="height: 36px">
+          <span style="line-height: 24px;" v-html="operator.description"></span>
         </td>
       </tr>
 
