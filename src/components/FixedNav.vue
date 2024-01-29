0b814d32da1f4876fe19e64fdecd40b314cc485c
MilkWind <2250957906@qq.com>
2024-01-30 02:22:24 +0800

    【自定义组件】适配暗黑模式，添加动态调节垂直居中位置功能
    Signed-off-by: MilkWind <2250957906@qq.com>

--- src/components/FixedNav.vue
+++ src/components/FixedNav.vue
@@ -4,7 +4,7 @@
   引入时，尽量在<template>末尾引入，避免页面元素将导航栏给遮住
   需要注意的是，若class为“op_title_ctext”的元素数量不多于1个，则该组件不显示
   -->
-  <div v-if="sections.length > 1" class="fixed-nav">
+  <div ref="fixedNav" v-if="sections.length > 1" class="fixed-nav">
     <div class="nav-content">
       <el-link v-for="section in sections" :key="section.text" @click="scrollTo(section.element)" :underline="false">
         {{ section.text }}
@@ -14,13 +14,13 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue';
+import {ref, onMounted, onUnmounted, nextTick} from 'vue';
 
+const sections = ref([]);
+const fixedNav = ref(null);
 const scrollTo = (element) => {
   element.scrollIntoView({behavior: 'smooth'});
 };
-
-const sections = ref([]);
 
 const fetchSections = () => {
   const elements = document.querySelectorAll('.op_title_ctext');
@@ -30,8 +30,25 @@ const fetchSections = () => {
   }));
 };
 
+const adjustNavPosition = () => {
+  if (fixedNav.value) {
+    const navHeight = fixedNav.value.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const topOffset = (windowHeight - navHeight) / 2;
+    fixedNav.value.style.top = `${topOffset}px`;
+  }
+};
+
 onMounted(() => {
   fetchSections();
+  nextTick().then(() => {
+    adjustNavPosition(); //确保该函数在所有DOM更新完成后才被调用
+  });
+  window.addEventListener('resize', adjustNavPosition);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', adjustNavPosition);
 });
 </script>
 
@@ -39,16 +56,16 @@ onMounted(() => {
 .fixed-nav {
   position: fixed;
   right: 0;
-  top: 43%;
 }
 
 .nav-content {
-  background-color: white;
-  border: 1px solid #ccc;
+  background-color: var(--c-background-color);
+  color: var(--c-text-color);
+  border: 2px solid var(--c-border-color);
   border-radius: 5px;
   padding: 10px;
   transition: transform 0.3s;
-  transform: translateX(105%); /* 当鼠标悬停在fixed-nav上时显示 */
+  transform: translateX(105%);
 }
 
 .fixed-nav:hover .nav-content {
