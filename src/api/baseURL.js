a36eaaed4bfe02297ac428a19d8ef3f44a065376
yamazakura <1969314158@qq.com>
2024-02-13 15:27:49 +0800

    api变更

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,4 +1,4 @@
-// const http = "https://ark.yituliu.cn/backend/";
-const http = "http://127.0.0.1:10010/";
+const http = "https://ark.yituliu.cn/backend/";
+// const http = "http://127.0.0.1:10010/";
 
 export { http };
