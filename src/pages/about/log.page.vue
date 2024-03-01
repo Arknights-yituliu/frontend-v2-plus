9f4710aed5ac98a0452a1c8a1544d2a2babd054d
yamazakura <1969314158@qq.com>
2024-03-01 18:07:59 +0800

    refactor:日志页面使用git commit_log 生成log

--- src/pages/about/log.page.vue
+++ src/pages/about/log.page.vue
@@ -1,19 +1,45 @@
 <script setup>
 import logTable from '/src/static/json/log.json'
-import {ref} from "vue";
+
 
 const logType = {
-  "新增":"新增",
-  "调整":"调整",
-  "修改":"修改",
-  "替换":"替换",
-  "优化":"优化",
-  "更新":"更新",
-  "修复":"修复",
-  "测试":"测试",
-  "重构":"重构",
+  "新增": "feat",
+  "调整": "update",
+  "修改": "update",
+  "替换": "update",
+  "优化": "perf",
+  "更新": "update",
+  "修复": "fix",
+  "测试": "test",
+  "重构": "refactor",
 }
 
+
+let logCollect = []
+
+for (const log of logTable) {
+  let logItem = logCollect[logCollect.length-1]
+  if (logItem) {
+    if (logItem.date !== log.date) {
+      logItem = {
+        date: log.date,
+        logs: []
+      }
+      logItem.logs.push(log)
+      logCollect.push(logItem)
+    } else {
+      logItem.logs.push(log)
+      logCollect[logCollect.length-1] = logItem
+    }
+  } else {
+    logItem = {
+      date: log.date,
+      logs: []
+    }
+    logItem.logs.push(log)
+    logCollect.push(logItem)
+  }
+}
 
 
 
@@ -23,13 +49,14 @@ const logType = {
 
   <div class="log-list">
     <h1>网站维护日志</h1>
-    <div class="log-item" v-for="({date,logs},index) in logTable" :key="index">
+    <div class="log-item" v-for="({date,logs},index) in logCollect" :key="index">
       <h2>{{ date }}</h2>
       <ul>
-        <li class="log-content" v-for="(log,index) in logs" :key="index" >
-          <span >{{ `【${log.tag}】` }}</span>
-          <span >{{ log.text }}</span>
-          <span >@{{ log.author }}</span>
+        <li class="log-content" v-for="(log,index) in logs" :key="index">
+          <span>{{ `${log.type}:` }}</span>
+          <span>{{ `${log.module}` }}</span>
+          <span>{{ log.description }}</span>
+          <span>@{{ log.author }}</span>
         </li>
       </ul>
     </div>
@@ -47,8 +74,8 @@ const logType = {
   display: flex;
 }
 
-.log-content span{
-  padding:0 8px;
+.log-content span {
+  padding: 0 8px;
 }
 
 .log-item {
@@ -66,7 +93,6 @@ const logType = {
 .log-author {
   width: 200px;
 }
-
 
 
 </style>
