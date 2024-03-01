244062451c0068859d9bc757dc7a5f7938134fe1
yamazakura <1969314158@qq.com>
2024-03-01 11:46:15 +0800

    feat:打印设备信息

--- src/pages/tools/gachaCal.page.client.vue
+++ src/pages/tools/gachaCal.page.client.vue
@@ -940,12 +940,15 @@
                 许可协议</a>进行许可。转载、公开或以任何形式复制、发行、再传播本页任何内容时，必须注明从明日方舟一图流转载，并提供版权标识、许可协议标识、免责标识和作品链接；且未经许可，不得将本站内容或由其衍生作品用于商业目的。<br/>
                 本项目为无偿开源项目，致力于方便明日方舟玩家。如有开发/数据分析/设计/美工经验，欢迎来开发群一叙。
               </div>
+              <div>设备宽度：{{deviceInfo.screenWidth}}；设备高度：{{deviceInfo.screenHeight}}</div>
+              <div>视口高度：{{deviceInfo.width}}；视口高度：{{deviceInfo.height}}</div>
             </div>
           </el-collapse-item>
         </el-collapse>
         <!-- <foot></foot> -->
       </el-col>
     </el-row>
+
   </div>
 </template>
 
@@ -1052,6 +1055,7 @@ export default {
       LMDCost: 0,
       pieData: [],
       current_pool_end_time: '',
+      deviceInfo:{height:0,width:0,screenHeight:0,screenWidth:0},
       schedules: {
         '彩六二期(3.21)': {
           display: true,
@@ -1084,6 +1088,11 @@ export default {
     this.timeSelector = '彩六二期(3.21)'
     this.getPackDataList()
     this.checkEndDate();
+
+    this.deviceInfo.height = window.innerHeight
+    this.deviceInfo.width = window.innerWidth
+    this.deviceInfo.screenHeight = window.screen.height
+    this.deviceInfo.screenWidth = window.screen.width;
 
 
     myChart = echarts.init(document.getElementById("gacha_total_pie"));
