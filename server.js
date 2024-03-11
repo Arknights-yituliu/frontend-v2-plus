63c39fbb1d1ef76b6370e2c463a17195c74b5020
yamazakura <1969314158@qq.com>
2024-03-11 16:16:06 +0800

    refactor(全页面):移动端适配

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
 import history  from 'connect-history-api-fallback'
-import { createSSRApp } from 'vue'
-import { renderToString } from 'vue/server-renderer'
 import express from "express";
 
 const server = express()
 const port = 3000
 
-import stage from '/src/pages/material/stageV3.page.vue'
 
-server.get('/', (req, res) => {
-    const app = createSSRApp(stage)
-
-    renderToString(app).then((html) => {
-        res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <title>Vue SSR Example</title>
-      </head>
-      <body>
-        <div id="app">${html}</div>
-      </body>
-    </html>
-    `)
-    })
-})
-
+server.use(express.static('dist'))
 
 server.use(history())
-server.use(express.static('dist'))
+
+
 
 server.listen(port, () => console.log(`服务于端口 ${port} 开启成功!`))
