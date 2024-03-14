e77f7804ced60a49e2bcceccc04a658f33366e6f
yamazakura <1969314158@qq.com>
2024-03-14 17:04:39 +0800

    refactor(全页面):移动端适配

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server:{
-    host:'0.0.0.0'
-  },
-  build: {
-    target: "es2015",
-    cssTarget: "chrome61",
-    rollupOptions: {},
-  },
+    plugins: [vue({
+        template: {
+            compilerOptions: {
+                // 将所有带短横线的标签名都视为自定义元素
+                isCustomElement: (tag) => tag.includes('-')
+            }
+        }
+    })],
+    server: {
+        host: '0.0.0.0'
+    },
+    build: {
+        target: "es2015",
+        cssTarget: "chrome61",
+        rollupOptions: {},
+    },
 })
 
