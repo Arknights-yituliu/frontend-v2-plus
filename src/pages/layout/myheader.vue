5d2a5b4d7fd3babbdc4c32436ccd6006076d5c69
yamazakura <1969314158@qq.com>
2024-06-08 23:03:44 +0800

    fix(导航):修复顶栏显示的页面名称某些情况下不显示的问题

--- src/pages/layout/myheader.vue
+++ src/pages/layout/myheader.vue
@@ -203,7 +203,7 @@ function openNewPage(url) {
             <div class="side-website-title">明日方舟一图流</div>
           </a>
           <!-- 导航菜单 -->
-          <div class="aside_menu_set" v-for="(parent, index) in LinkedTable" :key="index">
+          <div class="aside_menu_set" v-for="(parent, index) in LinkedTable" :key="index" v-show="index!=='backend'">
             <!-- 一级标题 -->
             <router-link :to="parent.path"  class="nav-bar nav-bar-parent">
               <div class="nav-bar-parent-icon"></div>
@@ -213,6 +213,7 @@ function openNewPage(url) {
             <!-- 二级标题组 -->
             <router-link :to="child.path" :href="child.path" class="nav-bar nav-bar-child"
                          v-for="(child,index) in parent.child" :key="index">
+
               <i class="iconfont menu-icon" :class="`icon-${child.icon}`"></i>
               <span>{{ child.text }}</span>
             </router-link>
