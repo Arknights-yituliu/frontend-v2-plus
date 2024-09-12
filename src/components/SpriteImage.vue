0672c544d92855397a07ba6dea4dc93280faf6bb
yamazakura <1969314158@qq.com>
2024-09-12 18:37:03 +0800

    feat(自定义组件):雪碧图调用组件增加圆角设置

--- src/components/SpriteImage.vue
+++ src/components/SpriteImage.vue
@@ -18,9 +18,16 @@ function calculatedSize() {
     ratio = 0.7
   }
 
+
+
   const displaySize = props.displaySize ? props.displaySize * ratio : 40;
 
-  wrapStyle.value = `position: relative;width: ${displaySize}px;height: ${displaySize}px;overflow: hidden;`
+  let roundedCornerStyle = ''
+  if(props.roundedCorner){
+    roundedCornerStyle = `border-radius:${props.roundedCorner}px;`
+  }
+  wrapStyle.value = `position: relative;width: ${displaySize}px;height: ${displaySize}px;overflow: hidden;${roundedCornerStyle}`
+
   const originalSize = props.originalSize
   spriteStyle.value = `position: absolute;transform: scale(${displaySize / originalSize});
   top: ${(displaySize - originalSize) / 2}px;left: ${(displaySize - originalSize) / 2}px;`
