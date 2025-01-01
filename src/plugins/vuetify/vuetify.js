b5951bf3dd9a9126897c8d3bc77cd1ca5be02b86
yamazakura <1969314158@qq.com>
2025-01-01 17:37:05 +0800

    icon(导航栏图标):更新导航栏图标

--- src/plugins/vuetify/vuetify.js
+++ src/plugins/vuetify/vuetify.js
@@ -1,6 +1,7 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const myCustomLightTheme = {
