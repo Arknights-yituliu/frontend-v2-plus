07ea3df9241c39610c47a8a1c5c9c572a92bc368
yamazakura <1969314158@qq.com>
2025-01-03 10:18:11 +0800

    feat(dps计算器):新坑暂时不知道咋填

--- src/pages/tools/dps-calculator.vue
+++ src/pages/tools/dps-calculator.vue
@@ -1,8 +1,19 @@
-<script setup> </script>
+<script setup>
+import '/src/assets/css/tool/dps_calculator.scss'
+
+</script>
 
 <template>
+  <div class="dps-calculator-page">
+    <v-card title="DPS计算" class="operator-card">
+      <v-card-text>
+        <v-list>
+          <v-list-item title="选择干员">
 
-  <v-card>
+          </v-list-item>
+        </v-list>
 
-  </v-card>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
