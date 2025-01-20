112c501f736dcfe4ad4780ac3d8d643492c66741
yamazakura <1969314158@qq.com>
2025-01-20 20:02:39 +0800

    fix(备案号):备案号没加上

--- src/App.vue
+++ src/App.vue
@@ -217,6 +217,12 @@ onMounted(() => {
           <router-view>
 
           </router-view>
+
+            <div class="flex justify-center align-center">
+              <img src="/image/website/备案图标.png" style="width: 20px; height: 20px;margin: 0 12px" alt=""/>
+              <a href="https://beian.miit.gov.cn/">豫ICP备2024043594号-1</a>
+            </div>
+
           <ComponentsContainer></ComponentsContainer>
         </v-container>
       </v-main>
