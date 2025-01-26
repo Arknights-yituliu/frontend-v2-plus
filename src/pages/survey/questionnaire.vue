191c053923304676a179b161884cc79dbb2cfa71
yamazakura <1969314158@qq.com>
2025-01-27 00:31:27 +0800

    feat(开荒干员调查):重新启用这个项目

--- src/pages/survey/questionnaire.vue
+++ src/pages/survey/questionnaire.vue
@@ -6,6 +6,7 @@ import '/src/assets/css/survey/questionnaire.scss'
 import {cMessage} from "/src/utils/message.js";
 import operatorDataAPI from '/src/api/operatorData.js'
 import questionnaireAPI from "/src/api/questionnaire.js";
+import OperatorAvatar from "@/components/sprite/OperatorAvatar.vue";
 
 let operatorGroupByProfession = new Map()
 
@@ -27,14 +28,15 @@ operatorGroupByProfession.forEach((v, k) => {
   v.sort((a, b) => b.rarity - a.rarity)
 })
 
-let operatorListByProfession = ref([])
+let displayOperatorList = ref([])
 let operatorTeam = ref([])
 let charIdList = ref([])
 let selectedProfession = ref('')
+let placeholder = ref(12)
 
 function chooseOperatorProfession(profession) {
   selectedProfession.value = profession
-  operatorListByProfession.value = operatorGroupByProfession.get(profession)
+  displayOperatorList.value = operatorGroupByProfession.get(profession)
 }
 
 function chooseOperator(operator) {
@@ -50,12 +52,14 @@ function chooseOperator(operator) {
     }
   }
   operatorTeam.value.push(operator)
+  placeholder.value--
   getCharIdList()
 }
 
 
 function removeOperator(operator) {
   operatorTeam.value = operatorTeam.value.filter(e => e.charId !== operator.charId)
+  placeholder.value++
   getCharIdList()
 }
 
@@ -200,142 +204,77 @@ onMounted(() => {
 
     <!-- <el-header>Header</el-header> -->
 
-    <h1>你会选择哪些干员加入开荒队？</h1>
-    <li>选择你的开荒干员，看看有多少博士所见略同！</li>
-    <el-divider style="margin: 12px 0px;"></el-divider>
-    <el-alert title="选择12位干员作为主力开荒队" type="info" :closable="false"
-              description="如难以取舍，可再选择0-4位干员作为替补"
-              style="margin: 8px 0px;"/>
-    <!-- 下面一条提示记录登陆后不可见 -->
-    <el-alert title="登录可使用更多功能" type="info" :closable="false"
-              description="可在不同设备间同步，筛选已有/已精二的干员"
-              style="margin: 8px 0px;"/>
-    <el-alert title="您还未提交过本答卷" type="success" :closable="false" description="提交后可查看其他博士们的选择"
-              style="margin: 8px 0px;"/>
-    <el-alert title="您上次提交答卷是14天前" type="success" :closable="false" description="大数据仅统计60天内的答卷"
-              style="margin: 8px 0px;"/>
-    <!-- 若大于60天 -->
-    <el-alert title="您上次提交答卷是114天前" type="warning" :closable="false"
-              description="大数据仅统计60天内的答卷，您的答卷将保留，但不参与统计"
-              style="margin: 8px 0px;"/>
-    <!-- 干员选择区域 -->
-    <div class="teams-area">
-      <div class="team-box" style="padding-right:0px;">
-        <div class="operator-team" id="team12">
-          <div v-for="(operator, index) of operatorTeam" :key="index" class="operator-team-item">
-            <div class="team-operator-avatar" @click="removeOperator(operator)">
-              <div :class="`bg-${operator.charId}`"
-                   style="box-shadow: 4px 4px 8px #00000080;">
-              </div>
+    <!--    <h1>你会选择哪些干员加入开荒队？</h1>-->
+    <!--    <p>选择你的开荒干员，看看有多少博士所见略同！</p>-->
+    <!--    <v-divider ></v-divider>-->
+    <!--    <el-alert title="选择12位干员作为主力开荒队" type="info" :closable="false"-->
+    <!--              description="如难以取舍，可再选择0-4位干员作为替补"-->
+    <!--              style="margin: 8px 0px;"/>-->
+    <!--    &lt;!&ndash; 下面一条提示记录登陆后不可见 &ndash;&gt;-->
+    <!--    <el-alert title="登录可使用更多功能" type="info" :closable="false"-->
+    <!--              description="可在不同设备间同步，筛选已有/已精二的干员"-->
+    <!--              style="margin: 8px 0px;"/>-->
+    <!--    <el-alert title="您还未提交过本答卷" type="success" :closable="false" description="提交后可查看其他博士们的选择"-->
+    <!--              style="margin: 8px 0px;"/>-->
+    <!--    <el-alert title="您上次提交答卷是14天前" type="success" :closable="false" description="大数据仅统计60天内的答卷"-->
+    <!--              style="margin: 8px 0px;"/>-->
+    <!--    &lt;!&ndash; 若大于60天 &ndash;&gt;-->
+    <!--    <el-alert title="您上次提交答卷是114天前" type="warning" :closable="false"-->
+    <!--              description="大数据仅统计60天内的答卷，您的答卷将保留，但不参与统计"-->
+    <!--              style="margin: 8px 0px;"/>-->
+
+    <v-alert
+        title="Alert title"
+        type="info"
+        class="m-12-0"
+    >
+
+      <p>1.提交的干员数量必须大于6人</p>
+      <p>2.由于提交的博士每个人的玩法差异较大，故本问卷的调查结果仅供参考，并不能认为是一个权威榜单</p>
+
+    </v-alert>
+
+    <div class="operator-form-and-checkbox">
+      <v-card title="选择12位你开荒时会优先选择的干员" class="operator-form">
+        <v-card-text>
+          <div class="flex flex-wrap justify-center">
+            <div v-for="(operator, index) of operatorTeam" :key="index" class="operator-team-item"
+                 @click="removeOperator(operator)">
+              <OperatorAvatar :char-id="operator.charId" size="80" mobile-size="80"></OperatorAvatar>
               <img src="/image/icon/cancel.png" alt="" class="cancel-icon">
+
+            </div>
+            <div v-for="x in placeholder" class="operator-team-item item-placeholder">
+              未选择
             </div>
           </div>
-          <div v-for="x in 12" class="operator-team-item item-placeholder">
-            未选择
+          <div class="flex justify-center">
+            <v-btn color="primary" text="上传编队"></v-btn>
           </div>
-        </div>
-        <div style="padding-left: 8px;">开荒编队 ( {{ operatorTeam.length }} / 12 ) *至少选择8位干员</div>
-      </div>
-      <!--          <div class="team_area" style="padding-left:0px;background-color: #707070b0;">-->
-      <!--            <div class="operator-team" id="team4">-->
-      <!--              <div v-for="x in 4" class="operator-team-item item_placeholder">-->
-      <!--                这是个占位符-->
-      <!--              </div>-->
-      <!--            </div>-->
-      <!--            <div style="padding-left: 8px;">替补干员 ( 0 / 4 )</div>-->
-      <!--          </div>-->
+        </v-card-text>
+
+      </v-card>
+
+      <v-card class="operator-checkbox">
+        <v-card-text>
+          <div>
+            <img :src="`/image/survey/bg/${p.value}.png`" alt=""
+                 class="profession-option"
+                 v-for="(p, index) in professionDict"
+                 @click="chooseOperatorProfession(p.value)">
+          </div>
+          <div class="flex flex-wrap justify-center">
+            <div v-for="(operator, profession) of displayOperatorList" :key="profession" class="operator-option"
+                 @click="chooseOperator(operator)" :class="selectedOperatorClass(operator.charId)">
+              <OperatorAvatar :char-id="operator.charId" size="60" mobile-size="60"></OperatorAvatar>
+            </div>
+          </div>
+        </v-card-text>
+      </v-card>
     </div>
-    <!-- 操作区 -->
-
-    <el-button type="primary" :disabled="operatorTeam.length<8" @click="uploadSubmitContent()" style="margin: 4px 0px;">选好了！</el-button>
-    <!--        <el-button type="primary" style="margin: 4px 0px;">选好了！</el-button>-->
-
-    <!-- 这里塞一个筛选器，代码比较冗长，或许可以重写一下？ -->
-    <div id="profession_filter" style="display: flex;">
-      <div class="profession-filter-single" v-for="(p, index) in professionDict"
-           @click="chooseOperatorProfession(p.value)">
-        <img :src="`/image/survey/bg/${p.value}.png`" style="width:46px" alt="">
-        <div class="profession-mask" v-show="p.value===selectedProfession"></div>
-        <div class="profession-count">0</div>
-      </div>
-
-      <div id="profession_options">
-        <el-checkbox value="只显示已有干员" border size=large type="info" round style="margin: 12px 8px;"/>
-        <el-checkbox value="只显示已精二干员" border size=large round style="margin: 12px 8px;"/>
-      </div>
-    </div>
-    <!-- 选择模块 -->
-    <div class="operator-checkbox">
-      <!--          &lt;!&ndash; 示例干员 &ndash;&gt;-->
-      <!--          <div class="operator-option operator_selected">-->
-      <!--            <div class="operator-avatar">-->
-      <!--              <div class="bg-char_1013_chen2"></div>-->
-      <!--            </div>-->
-      <!--            <div class="operator_name_shell">-->
-      <!--              <div class="operator_name">被选中的干员</div>-->
-      <!--            </div>-->
-      <!--          </div>-->
-      <!--          &lt;!&ndash; 示例干员2 &ndash;&gt;-->
-      <!--          <div class="operator-option operator_selected">-->
-      <!--            <div class="operator-avatar">-->
-      <!--              <div class="bg-char_1013_chen2"></div>-->
-      <!--            </div>-->
-      <!--            <div class="operator_name_shell">-->
-      <!--              <div class="operator_name">被选中的八字干员</div>-->
-      <!--            </div>-->
-      <!--          </div>-->
-      <!-- 正式区域 -->
-      <div v-for="(operator, profession) of operatorListByProfession" :key="profession" class="operator-option"
-           @click="chooseOperator(operator)" :class="selectedOperatorClass(operator.charId)">
-        <div class="operator-avatar">
-          <div :class="`bg-${operator.charId}`"></div>
-        </div>
-        <div class="operator-name">{{ operator.name }}</div>
-        <!-- <span></span> -->
-        <!-- <span>选择率：50%</span> -->
-      </div>
-    </div>
-
-    <!--    <div class="survey-result-table">-->
-    <!--      <div class="survey-result-table-title">-->
-    <!--        <div class="survey-result-table-tr">-->
-    <!--          <div class="survey-result-table-operator-name" >干员名称</div>-->
-    <!--          <div class="w-100">开荒上场率</div>-->
-    <!--          <div class="w-110">开荒平均练度</div>-->
-    <!--          <div class="w-100">干员持有率</div>-->
-    <!--        </div>-->
-    <!--        <div class="survey-result-table-tr">-->
-    <!--          <div class="survey-result-table-operator-name" >干员名称</div>-->
-    <!--          <div class="w-100">开荒上场率</div>-->
-    <!--          <div class="w-110">开荒平均练度</div>-->
-    <!--          <div class="w-100">干员持有率</div>-->
-    <!--        </div>-->
-    <!--      </div>-->
-    <!--      <div class="survey-result-table-content">-->
-    <!--        <div class="survey-result-table-content-item" v-for="(list,index) in listOperatorSlice" :key="index">-->
-    <!--          <div v-for="(operator,index) in list" :key="index" class="survey-result-table-tr">-->
-    <!--            <div class="survey-result-table-operator-name"  style="">-->
-    <!--              <div class="survey-result-table-avatar">-->
-    <!--                <div :class="`bg-${operator.charId}`"></div>-->
-    <!--                <img :src="`/image/survey/bg/${operator.profession}.png`"-->
-    <!--                     class="profession-icon-w-20" alt="" >-->
-    <!--                <span>-->
-    <!--                 {{operator.name}}-->
-    <!--              </span>-->
-    <!--              </div>-->
-
-    <!--            </div>-->
-    <!--            <div class="w-100">{{ operator.appearanceRate.toFixed(1) }} %</div>-->
-    <!--            <div class="w-110">精英2 Lv.30</div>-->
-    <!--            <div class="w-100">{{ (operator.own * 100).toFixed(1) }} %</div>-->
-    <!--          </div>-->
-    <!--        </div>-->
-    <!--      </div>-->
-    <!--    </div>-->
-
 
     <!-- 个人结果展示模块 -->
-    <div id="survey_result_personal" class="survey-result-personal" >
+    <div id="survey_result_personal" class="survey-result-personal" style="display: none">
       <div class="survey-result-operator" v-for="(operator,index) in listOperators.slice(0,12)" :key="index">
         <div class="survey-result-avatar" style="">
           <div :class="`bg-${operator.charId}`">
@@ -365,18 +304,18 @@ onMounted(() => {
     <!-- 先大概看下统计结果，然后看看怎么划段分级 -->
 
 
-    <div class="survey-ranking-table" style="display: none">
-      <div v-for="(group,index) in listOperatorGroupByAttendance" :key="index" class="survey-ranking-tier">
-        <div class="survey-ranking-tier-interval" :style="`background-color: ${colorList[index]}`">
-          {{ group.interval }}
-        </div>
-        <div class="survey-ranking-tier-operators">
-          <div v-for="(operator,index) in group.list" :key="index" class="survey-ranking-tier-avatar">
-            <div :class="`bg-${operator.charId}`"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!--    <div class="survey-ranking-table" >-->
+    <!--      <div v-for="(group,index) in listOperatorGroupByAttendance" :key="index" class="survey-ranking-tier">-->
+    <!--        <div class="survey-ranking-tier-interval" :style="`background-color: ${colorList[index]}`">-->
+    <!--          {{ group.interval }}-->
+    <!--        </div>-->
+    <!--        <div class="survey-ranking-tier-operators">-->
+    <!--          <div v-for="(operator,index) in group.list" :key="index" class="survey-ranking-tier-avatar">-->
+    <!--            <div :class="`bg-${operator.charId}`"></div>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
 
   </div>
 
