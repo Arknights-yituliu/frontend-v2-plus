4a72084df273dff1e8eba0b5d52058c6d99e4cb2
yamazakura <1969314158@qq.com>
2025-03-07 14:37:21 +0800

    update(干员练度统计):更新干员数据

---
+++ src/pages/dev/operator-add-time.vue
@@ -0,0 +1,30 @@
+<script setup>
+import operatorAddTime from '/src/static/json/operator/operator_add_time.json'
+
+
+</script>
+<template>
+  <table class="add-table">
+    <tbody>
+    <tr v-for="(item,name) in operatorAddTime">
+      <td >
+          {{name}}
+      </td>
+      <td>
+        {{item.addTime}}
+      </td>
+    </tr>
+    </tbody>
+  </table>
+
+</template>
+
+<style>
+.add-table{
+  border-collapse: collapse;
+  td{
+    border: 1px solid gray;
+    padding: 12px;
+  }
+}
+</style>
