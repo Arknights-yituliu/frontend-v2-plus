f8e1880a5a074c365c77ed41bb2b5b5e2102d033
yamazakura <1969314158@qq.com>
2025-04-16 00:04:15 +0800

    fix(干员练度调查结果):修复因干员练度调查结果数据结构变更导致的页面数据无法正常加载

--- src/utils/indexedDB/operatorProgressionStatisticsData.js
+++ src/utils/indexedDB/operatorProgressionStatisticsData.js
@@ -11,12 +11,12 @@ async function getData() {
     const cacheKey = 'operatorProgressionStatisticsV4'
     let cacheData = await myDatabase.cache_data.get(cacheKey)
 
-    // if (cacheData && cacheData.resource.result.length) {
-    //     if (new Date().getTime() - cacheData.createTime < 60 * 60 * 24 * 1000) {
-    //         console.log(cacheKey, '返回缓存的数据')
-    //         return cacheData.resource
-    //     }
-    // }
+    if (cacheData && cacheData.resource.result.length) {
+        if (new Date().getTime() - cacheData.createTime < 60 * 60 * 24 * 1000) {
+            console.log(cacheKey, '返回缓存的数据')
+            return cacheData.resource
+        }
+    }
 
     await operatorDataAPI.getOperatorStatisticsResult().then(response => {
         console.log(cacheKey, '返回来自服务器的数据')
