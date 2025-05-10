14d3bccd074ab67ac65a59b0d2d684fbe97f5894
yamazakura <1969314158@qq.com>
2025-05-10 18:28:52 +0800

    fix(自定义组件):修复一些样式错误的问题

--- src/utils/message.js
+++ src/utils/message.js
@@ -1,6 +1,7 @@
-const style = {
+const BaseStyle = {
     opacity: '0',
-    minWidth: '240px',
+    // minWidth: '240px',
+    width:'fit-content',
     borderRadius: '4px',
     lineHeight: '24px',
     padding: '4px 12px',
@@ -8,18 +9,20 @@ const style = {
     position: 'fixed',
     top: '0px',
     fontSize: '14px',
+    display: "flex",
     left: '50%',
+    margin: 'auto',
     fontWeight: '600',
     zIndex: '3000',
     transition: 'opacity 0.3s, top 0.5s',
-    transform: 'translate(-50%, 0)',
-    border:'1px solid #a8ffc1'
+    transform: 'translate(-50%)',
+    border: '1px solid #a8ffc1'
 }
 
 const colorStyle = {
     success: {
         color: '#4CAF50',
-        background: '#E8F5E9',
+        background: '#ecf6ed',
         borderColor: '#4CAF50' // 深绿色
     },
     warn: {
@@ -29,18 +32,17 @@ const colorStyle = {
     },
     error: {
         color: '#FF4E4EFF',
-        background: '#F7E3E3FF',
+        background: '#f8ecec',
         borderColor: '#ffc5c5' // 深红色
     },
     info: {
         color: '#2196F3',
-        background: '#E3F2FD',
-        borderColor: '#E3F2FD' // 深红色
+        background: '#f2f9fd',
+        borderColor: '#bee9ff' // 深红色
     }
 };
 
 // const color = '#a8ffc1'
-
 
 
 let send = 1;
@@ -48,44 +50,56 @@ let messageBars = [];
 
 /**
  *
- * @param config  消息配置
+ * @param {{type:string,text:string,duration:number}} config  消息配置
  * @example
+ * 传入参数
  * {
  * type:消息类型,
  * text:消息内容,
  * duration:持续时间
  * }
  */
-function createMessage(config){
-    let {text,duration,type} = config;
-    if(!duration){
+function createMessage(config) {
+    let {text, duration, type} = config;
+    if (!duration) {
         duration = 4000;
     }
 
 
     send++;
+
+    //创建一个message元素
     let messageBar = document.createElement("div");
 
-    for (const property in style) {
-        // messageBar.style.overflow = 'hidden'
-        messageBar.style[property] = style[property]
+    //赋予message元素基础样式
+    for (const property in BaseStyle) {
+        messageBar.style[property] = BaseStyle[property]
     }
 
+    //赋予message元素的特殊样式
+    for (const property in colorStyle[type]) {
+        messageBar.style[property] = colorStyle[type][property]
+    }
 
+    //赋予message元素独立id
     messageBar.id = "messageBar" + send;
     messageBars.push("messageBar" + send);
 
     // const textElement = document.createTextNode(text);
     // messageBar.appendChild(textElement);
+
+    //向message元素写入文本
+
     messageBar.textContent = text
     document.body.appendChild(messageBar);
 
+    // const componentsContainer = document.getElementById("components-container-w1i3dqk");
+    // //将message元素加入根元素
+    // componentsContainer.appendChild(messageBar);
 
-    for (const property in colorStyle[type]) {
-        messageBar.style[property] = colorStyle[type][property]
-    }
 
-    setTimeout(function () {
+
+    setTimeout( ()=> {
         messageBar.style.opacity = '1'; //淡入
         for (let i in messageBars) {
             document.getElementById(messageBars[i]).style.top = 20 + i * 50 + "px";
@@ -93,11 +107,12 @@ function createMessage(config){
     }, 16);
 
     // 淡出
-    setTimeout(function () {
+    setTimeout(()=>  {
         messageBar.style.opacity = '0';
     }, duration - 300); // 在消息消失前300ms开始淡出，与transition保持一致
 
-    setTimeout(function () {
+    //在持续时间结束后将message元素销毁，将message列表中未过期的message元素向上移动
+    setTimeout(()=>  {
         messageBar.remove();
         messageBars = messageBars.splice(1);
         // console.log(messageBars)
@@ -113,9 +128,9 @@ function cMessage(text, type = 'success', duration = 4000) {
     send++;
     let messageBar = document.createElement("div");
 
-    for (const property in style) {
+    for (const property in BaseStyle) {
         // messageBar.style.overflow = 'hidden'
-        messageBar.style[property] = style[property]
+        messageBar.style[property] = BaseStyle[property]
     }
 
 
@@ -155,4 +170,4 @@ function cMessage(text, type = 'success', duration = 4000) {
 
 }
 
-export {cMessage,createMessage};
+export {cMessage, createMessage};
