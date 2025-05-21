f8c326b12fc47cb41201c5b3942554fc2b3c8d65
yamazakura <1969314158@qq.com>
2025-05-21 16:03:29 +0800

    feat(预设样式):将预设样式拆分

--- src/pages/dev/dev.vue
+++ src/pages/dev/dev.vue
@@ -1,11 +1,11 @@
 <script setup>
 
-
+import Atomic from "/src/pages/dev/atomic.vue";
 import CompositeTable from "@/pages/dev/composite_table.vue";
 </script>
 
 
 <template>
-<CompositeTable></CompositeTable>
-
+<!--<CompositeTable></CompositeTable>-->
+<Atomic></Atomic>
 </template>
