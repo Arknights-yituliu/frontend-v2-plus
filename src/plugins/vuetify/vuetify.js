38c99e941cf7915a07570a5d946a1a7552d182a3
yamazakura <1969314158@qq.com>
2025-05-23 19:06:15 +0800

    chore(组件图标):撤销将vuetify组件图标改为cdn引入的操作

--- src/plugins/vuetify/vuetify.js
+++ src/plugins/vuetify/vuetify.js
@@ -1,6 +1,7 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const myCustomLightTheme = {
