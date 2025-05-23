ac9e1f3e48d99e0d0cc64d2caca6439d4d0b33d8
yamazakura <1969314158@qq.com>
2025-05-23 17:59:03 +0800

    chore(组件图标):将vuetify组件图标改为cdn引入

--- docs/plugins/vuetify.js
+++ docs/plugins/vuetify.js
@@ -1,7 +1,6 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const myCustomLightTheme = {
