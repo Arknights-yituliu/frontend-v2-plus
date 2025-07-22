9550a5033f6afa6c1f72c2da09cef4821d56f521
yamazakura <1969314158@qq.com>
2025-07-22 16:19:38 +0800

    fix(精英化与专精性价比):跳过特勤干员radian

--- src/pages/material/elite/js/formatOperatorData.js
+++ src/pages/material/elite/js/formatOperatorData.js
@@ -83,6 +83,7 @@ export const createOperatorData = (charId, charInfo) => {
   const filteredElite = elite.filter(obj => Object.keys(obj).length !== 0)
   // 精英化材料数据里面没有龙门币数据, 添加精英化时消耗的龙门币数量
   // 因为目前只有4,5,6星干员精二券(也只有4,5,6星干员能精二), 所以仅对4,5,6星干员添加龙门币数量
+  console.log(charId)
   if (rarity > 3) {
     const baseEliteMaterials = operatorRarityBaseMaterialMap.get(rarity).elite
     baseEliteMaterials.forEach((rankMaterials, index) => {
@@ -235,6 +236,9 @@ const operatorInit = () => {
   fixedMaterialIds = [...baseMaterialIds, ...chipIds] // 合成固定材料id数组
   
   for (const [charId, charInfo] of operatorMap.entries()) {
+    if("char_4195_radian"===charId){
+      continue
+    }
     insertOperatorData(charId, charInfo)
   }
   initOperatorData()
