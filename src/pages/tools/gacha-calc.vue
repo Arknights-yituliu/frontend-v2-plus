2f14081929c719f34a363349c7a14116af92275d
Zirun-wang <zirunwang.g@gmail.com>
2025-08-12 11:17:39 +0800

    fix:微调排期

--- src/pages/tools/gacha-calc.vue
+++ src/pages/tools/gacha-calc.vue
@@ -252,7 +252,7 @@ const scheduleOptions = [
     disabled: false,
     dailyGiftResources: true,
     accuracyFlag: false,
-    historicalPackTimeRange: [new Date('2024/10/30 00:00:00').getTime(), new Date('2024/11/15 23:59:59').getTime(),]
+    historicalPackTimeRange: [new Date('2024/09/01 00:00:00').getTime(), new Date('2024/11/15 23:59:59').getTime(),]
   },
   {
     name: 'Ave Mujica联动',
