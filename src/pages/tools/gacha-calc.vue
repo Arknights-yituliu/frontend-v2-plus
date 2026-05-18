67a140896a0f232776660d0ec95c834b616a1eeb
Zirun-wang <zirunwang.g@gmail.com>
2026-05-18 18:36:34 +0800

    chore:文本微调

--- src/pages/tools/gacha-calc.vue
+++ src/pages/tools/gacha-calc.vue
@@ -280,7 +280,7 @@ const scheduleOptions = [
     historyEndTime: new Date("2023/03/28 04:00:00"),
   },
   {
-    name: "怪猎联动复刻",
+    name: "怪猎一期复刻",
     dateString: "(0615-0629)",
     start: new Date("2026/06/15 12:00:00"),
     end: new Date("2026/06/29 04:00:00"),
@@ -292,8 +292,8 @@ const scheduleOptions = [
     historyEndTime: new Date("2023/03/28 04:00:00"),
   },
    {
-    name: "怪猎联动二期+复刻",
-    dateString: "(0615-0629)",
+    name: "二期+复刻",
+    dateString: "(0601-0629)",
     start: new Date("2026/06/01 12:00:00"),
     end: new Date("2026/06/29 04:00:00"),
     activityType: "联动限定",
