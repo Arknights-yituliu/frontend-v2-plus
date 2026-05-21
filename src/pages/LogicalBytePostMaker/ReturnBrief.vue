dcd1f17e798d44172805d6f18cbe206512841e53
Zirun-wang <zirunwang.g@gmail.com>
2026-05-21 22:10:28 +0800

    fix:修复图片引用问题

--- src/pages/LogicalBytePostMaker/ReturnBrief.vue
+++ src/pages/LogicalBytePostMaker/ReturnBrief.vue
@@ -110,7 +110,7 @@
                 </div>
               </div>
               <div class="footer-image">
-                <img src="https://yituliu-dev-1307648010.cos.ap-beijing.myqcloud.com/image/website/QR/LogicalByteQR.png?q-sign-algorithm=sha1&q-ak=AKIDEpcgVogk4rlAG0uUqNmIbxGkHB2c-fZSPrhKJae_QsWKBmsS7zIM9HNMtc2mxSmU&q-sign-time=1773116074;1773119674&q-key-time=1773116074;1773119674&q-header-list=host&q-url-param-list=&q-signature=b9e546bf82f826a060854fcdc0a5a3063756d2fc&x-cos-security-token=XZiLyE60RcoeRbhoLkKT9Hkv7knzYMza8373400ba16a5183c4ec4f9b2dea3071TuKK-c_U9FCJX0kem7AsMITAqllPJwzA0HXlmHp6TKVKIE1cc5m83jQKrPLKM9yvzhyRz_PMaurjHeWcNZHDLhE2RSM-MX-ZwyvNmFPiFVVO7VC7-rH87SSB7s5xFjBWBT3BfGpjNoMZabi7foiFDeIC-bToPIWWg6MIQoBcfiZmnGeeQd9mpI1kXyOP2T_DvU_iZpME5KGrBe6oY3m8-5iBPEIz-MAqw9POKajK-DRk0WwoWk19A7GuyRaqO1bq0AVohPMiO6E4MGUblwqoLQ" alt="LogicalByte QR Code" />
+                <img src="/image/website/QR/LogicalByteQR.png" alt="LogicalByte QR Code" />
               </div>
             </div>
           </div>
